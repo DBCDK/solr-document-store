800b8c10d299a9486ff6b84c469e701ad69e616d
Jacob Poulsen <ja7@dbc.dk>
2017-11-08 10:13:08 +0100

    Initial Holdings entry point

---
+++ document-store-service/src/main/java/dk/dbc/search/solrdocstore/HoldingsItemBean.java
@@ -0,0 +1,41 @@
+package dk.dbc.search.solrdocstore;
+
+import dk.dbc.commons.jsonb.JSONBContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+
+@Stateless
+@Path("")
+public class HoldingsItemBean {
+        private static final Logger log = LoggerFactory.getLogger(HoldingsItemBean.class);
+
+        private final JSONBContext jsonbContext = new JSONBContext();
+
+        @PersistenceContext(unitName = "solrDocumentStore_PU")
+        EntityManager entityManager;
+
+        @POST
+        @Path("holdings")
+        @Consumes({ MediaType.APPLICATION_JSON })
+        @Produces({MediaType.APPLICATION_JSON})
+        public Response addBibliographicKeys(@Context UriInfo uriInfo, String KeyJsonContent) throws Exception {
+            log.info("AddBibliographicKeys called ");
+
+            HoldingsItemEntity hi=jsonbContext.unmarshall(KeyJsonContent, HoldingsItemEntity.class);
+
+            entityManager.merge( hi );
+
+            return Response.ok().entity("{ \"ok\": true }").build(); }
+}
