ad16f94eaf1f3904e5cb08f754914882f90ee9a1
Morten Bøgeskov <mb@dbc.dk>
2017-11-22 09:03:01 +0100

    PMD, findbugs, codestyle

--- service/src/main/java/dk/dbc/search/solrdocstore/AgencyItemKey.java
+++ service/src/main/java/dk/dbc/search/solrdocstore/AgencyItemKey.java
@@ -2,12 +2,16 @@ package dk.dbc.search.solrdocstore;
 
 import javax.persistence.Embeddable;
 import java.io.Serializable;
+import java.util.Objects;
 
 @Embeddable
 public class AgencyItemKey implements Serializable {
+
+    private static final long serialVersionUID = -2054293971622143423L;
+
     public int agencyId;
     public String bibliographicRecordId;
-    
+
     public AgencyItemKey withAgencyId(int agencyId) {
         this.agencyId = agencyId;
         return this;
@@ -17,4 +21,34 @@ public class AgencyItemKey implements Serializable {
         this.bibliographicRecordId = bibliographicRecordId;
         return this;
     }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 43 * hash + this.agencyId;
+        hash = 43 * hash + Objects.hashCode(this.bibliographicRecordId);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final AgencyItemKey other = (AgencyItemKey) obj;
+        if (this.agencyId != other.agencyId) {
+            return false;
+        }
+        if (!Objects.equals(this.bibliographicRecordId, other.bibliographicRecordId)) {
+            return false;
+        }
+        return true;
+    }
+
 }
--- service/src/main/java/dk/dbc/search/solrdocstore/DatabaseMigrator.java
+++ service/src/main/java/dk/dbc/search/solrdocstore/DatabaseMigrator.java
@@ -14,31 +14,33 @@ import javax.sql.DataSource;
 @Singleton
 @Startup
 public class DatabaseMigrator {
+
     private static final Logger LOGGER = LoggerFactory.getLogger(DatabaseMigrator.class);
 
     @Resource(lookup = "jdbc/solr-doc-store")
     DataSource dataSource;
 
-   	@PostConstruct
-   	public void migrate() {
-   		final Flyway flyway = new Flyway();
+    @PostConstruct
+    public void migrate() {
+        final Flyway flyway = new Flyway();
         flyway.setTable("schema_version");
         flyway.setBaselineOnMigrate(true);
-   		flyway.setDataSource(dataSource);
-   		for (MigrationInfo i : flyway.info().all()) {
-   			LOGGER.info("db task {} : {} from file '{}'", i.getVersion(), i.getDescription(), i.getScript());
-   		}
-   		flyway.migrate();
-   	}
+        flyway.setDataSource(dataSource);
+        for (MigrationInfo i : flyway.info().all()) {
+            LOGGER.info("db task {} : {} from file '{}'", i.getVersion(), i.getDescription(), i.getScript());
+        }
+        flyway.migrate();
+    }
 
-	public DatabaseMigrator( ) {
-	}
-	/**
-	 * For Integration test only
-	 * @param dataSource .
-	 */
-	public DatabaseMigrator(DataSource dataSource) {
-		this.dataSource = dataSource;
-	}
+    public DatabaseMigrator() {
+    }
+
+    /**
+     * For Integration test only
+     *
+     * @param dataSource .
+     */
+    public DatabaseMigrator(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 }
-
