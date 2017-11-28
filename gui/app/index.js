944d083401510dc79efd93545ae3dcf64edf6588
Peter Garnæs <pag@dbc.com>
2017-11-28 10:38:54 +0100

    Removed all cdn dependencies on styling, by bundling css depended on through npm

--- gui/app/index.js
+++ gui/app/index.js
@@ -1,6 +1,9 @@
 const React = require("react");
 const ReactDOM = require("react-dom");
 import App from './components/app';
+// Webpack will bundle styling
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.min.css';
 
 ReactDOM.render(
     <App/>,
