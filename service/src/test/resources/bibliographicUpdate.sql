d9e5da030d15b8854c97c90814948db4eac4ec6b
Jacob Poulsen <ja7@dbc.dk>
2017-11-29 15:06:22 +0100

    Simplified test data. Added test for update of non fbs libarray Extracted Libarary configuration to SinteltonBean for later lookup in OpenAgency

--- service/src/test/resources/bibliographicUpdate.sql
+++ service/src/test/resources/bibliographicUpdate.sql
@@ -1,31 +1,67 @@
 --- 4 items for different levels of records
 
 INSERT INTO bibliographicSolrKeys (AGENCYID, BIBLIOGRAPHICRECORDID, DELETED, INDEXKEYS, PRODUCERVERSION, TRACKINGID, UNIT, WORK)
-VALUES (870970, 'has970', FALSE, '{}' :: JSONB, '5544', 'track', 'unit:3', 'work:3');
+VALUES (870970, 'has970', TRUE, '{
+    "ti": [
+        "has970"
+    ]
+}' :: JSONB, '5544', 'track', 'unit:3', 'work:3');
 
 
 INSERT INTO bibliographicSolrKeys (AGENCYID, BIBLIOGRAPHICRECORDID, DELETED, INDEXKEYS, PRODUCERVERSION, TRACKINGID, UNIT, WORK)
-VALUES (870970, 'has300', FALSE, '{}' :: JSONB, '5544', 'track', 'unit:3', 'work:3');
+VALUES (870970, 'has300', TRUE, '{
+    "ti": [
+        "has970"
+    ]
+}' :: JSONB, '5544', 'track', 'unit:3', 'work:3');
 INSERT INTO bibliographicSolrKeys (AGENCYID, BIBLIOGRAPHICRECORDID, DELETED, INDEXKEYS, PRODUCERVERSION, TRACKINGID, UNIT, WORK)
-VALUES (300000, 'has300', FALSE, '{}' :: JSONB, '5544', 'track', 'unit:3', 'work:3');
+VALUES (300000, 'has300', TRUE, '{
+    "ti": [
+        "has970"
+    ]
+}' :: JSONB, '5544', 'track', 'unit:3', 'work:3');
 
 
 INSERT INTO bibliographicSolrKeys (AGENCYID, BIBLIOGRAPHICRECORDID, DELETED, INDEXKEYS, PRODUCERVERSION, TRACKINGID, UNIT, WORK)
-VALUES (870970, 'has700', FALSE, '{}' :: JSONB, '5544', 'track', 'unit:3', 'work:3');
+VALUES (870970, 'has700', TRUE, '{
+    "ti": [
+        "has970"
+    ]
+}' :: JSONB, '5544', 'track', 'unit:3', 'work:3');
 INSERT INTO bibliographicSolrKeys (AGENCYID, BIBLIOGRAPHICRECORDID, DELETED, INDEXKEYS, PRODUCERVERSION, TRACKINGID, UNIT, WORK)
-VALUES (300000, 'has700', FALSE, '{}' :: JSONB, '5544', 'track', 'unit:3', 'work:3');
+VALUES (300000, 'has700', TRUE, '{
+    "ti": [
+        "has970"
+    ]
+}' :: JSONB, '5544', 'track', 'unit:3', 'work:3');
 INSERT INTO bibliographicSolrKeys (AGENCYID, BIBLIOGRAPHICRECORDID, DELETED, INDEXKEYS, PRODUCERVERSION, TRACKINGID, UNIT, WORK)
-VALUES (700000, 'has700', FALSE, '{}' :: JSONB, '5544', 'track', 'unit:3', 'work:3');
+VALUES (700000, 'has700', TRUE, '{
+    "ti": [
+        "has970"
+    ]
+}' :: JSONB, '5544', 'track', 'unit:3', 'work:3');
 
 
 INSERT INTO bibliographicSolrKeys (AGENCYID, BIBLIOGRAPHICRECORDID, DELETED, INDEXKEYS, PRODUCERVERSION, TRACKINGID, UNIT, WORK)
-VALUES (870970, 'has700no300', FALSE, '{}' :: JSONB, '5544', 'track', 'unit:3', 'work:3');
+VALUES (870970, 'has700no300', TRUE, '{
+    "ti": [
+        "has970"
+    ]
+}' :: JSONB, '5544', 'track', 'unit:3', 'work:3');
 INSERT INTO bibliographicSolrKeys (AGENCYID, BIBLIOGRAPHICRECORDID, DELETED, INDEXKEYS, PRODUCERVERSION, TRACKINGID, UNIT, WORK)
-VALUES (700000, 'has700no300', FALSE, '{}' :: JSONB, '5544', 'track', 'unit:3', 'work:3');
+VALUES (700000, 'has700no300', TRUE, '{
+    "ti": [
+        "has970"
+    ]
+}' :: JSONB, '5544', 'track', 'unit:3', 'work:3');
 
 
 INSERT INTO bibliographicSolrKeys (AGENCYID, BIBLIOGRAPHICRECORDID, DELETED, INDEXKEYS, PRODUCERVERSION, TRACKINGID, UNIT, WORK)
-VALUES (700000, 'single700', FALSE, '{}' :: JSONB, '5544', 'track', 'unit:3', 'work:3');
+VALUES (700000, 'single700', TRUE, '{
+    "ti": [
+        "single"
+    ]
+}' :: JSONB, '5544', 'track', 'unit:3', 'work:3');
 
 -- Holdings keys
 
@@ -59,16 +95,17 @@ VALUES (700100, 'has970', '[]' :: JSONB, 'revision', 'track');
 
 
 INSERT INTO holdingsitemssolrkeys (AGENCYID, BIBLIOGRAPHICRECORDID, INDEXKEYS, PRODUCERVERSION, TRACKINGID)
-VALUES (700000, 'new', '[]' :: JSONB, 'revision', 'track');
-
-INSERT INTO holdingsitemssolrkeys (AGENCYID, BIBLIOGRAPHICRECORDID, INDEXKEYS, PRODUCERVERSION, TRACKINGID)
-VALUES (700100, 'new', '[]' :: JSONB, 'revision', 'track');
+VALUES (700000, 'new', '[
+    {
+        "id": [
+            "argle"
+        ]
+    }
+]' :: JSONB, 'revision', 'track');
 
 INSERT INTO holdingsitemssolrkeys (AGENCYID, BIBLIOGRAPHICRECORDID, INDEXKEYS, PRODUCERVERSION, TRACKINGID)
 VALUES (300100, 'new', '[]' :: JSONB, 'revision', 'track');
 
-INSERT INTO holdingsitemssolrkeys (AGENCYID, BIBLIOGRAPHICRECORDID, INDEXKEYS, PRODUCERVERSION, TRACKINGID)
-VALUES (300200, 'new', '[]' :: JSONB, 'revision', 'track');
 
 
 INSERT INTO holdingsitemssolrkeys (AGENCYID, BIBLIOGRAPHICRECORDID, INDEXKEYS, PRODUCERVERSION, TRACKINGID)
