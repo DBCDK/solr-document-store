7342a2e8979a34d16c3860b4e60af110c3bf80c1
Jacob Poulsen <ja7@dbc.dk>
2017-11-29 12:20:33 +0100

    Implemented review Comments    - Member name changes to BibliographicToBibliographic old/new -> decommissioned/current

--- service/src/main/java/dk/dbc/search/solrdocstore/BibliographicToBibliographicEntity.java
+++ service/src/main/java/dk/dbc/search/solrdocstore/BibliographicToBibliographicEntity.java
@@ -8,7 +8,7 @@ import javax.persistence.Table;
 @Table(name = "BibliographicToBibliographic")
 public class BibliographicToBibliographicEntity {
     @Id
-    public String oldRecordId;
+    public String decommissionedRecordId;
 
-    public String newRecordId;
+    public String currentRecordId;
 }
