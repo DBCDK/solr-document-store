6a23b6331802186c5631f99043b57abd64a9b43f
Michael Willer <willer@lundogbendsen.dk>
2017-12-07 12:05:15 +0100

    us-2246 Bean created and tested. Now rest-call.

--- service/src/test/java/dk/dbc/search/solrdocstore/AgencyLibraryTypeBeanIT.java
+++ service/src/test/java/dk/dbc/search/solrdocstore/AgencyLibraryTypeBeanIT.java
@@ -1,5 +1,6 @@
 package dk.dbc.search.solrdocstore;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -20,6 +21,36 @@ public class AgencyLibraryTypeBeanIT extends JpaSolrDocStoreIntegrationTester {
 
     @Test
     public void insertFindAndDelete(){
+        int dummyAgency = 1234;
+        LibraryConfig.LibraryType dummyType = LibraryConfig.LibraryType.FBSSchool;
+
+        persist(dummyAgency,dummyType, bean.entityManager);
+
+        LibraryConfig.LibraryType libraryType = bean.fetchAndCacheLibraryType(dummyAgency);
+        Assert.assertEquals(dummyType,libraryType);
+        remove(dummyAgency,bean.entityManager);
+
+        AgencyLibraryTypeEntity entity = findEntityWithKey(dummyAgency, bean.entityManager);
+        Assert.assertNull(entity);
+    }
+
+    private void remove(int agency, EntityManager em) {
+        AgencyLibraryTypeEntity entity = findEntityWithKey(agency, em);
+        env().getPersistenceContext().run( () -> em.remove(entity));
+    }
+
+    private void persist(int key, LibraryConfig.LibraryType fbs, EntityManager em) {
+        env().getPersistenceContext().run(() -> {
+            AgencyLibraryTypeEntity e = new AgencyLibraryTypeEntity();
+            e.agencyId = key;
+            e.libraryType = fbs.name();
+            em.persist(e);
+        });
+    }
+
+    private AgencyLibraryTypeEntity findEntityWithKey(int key, EntityManager em){
+        return env().getPersistenceContext().run(() -> em.find(AgencyLibraryTypeEntity.class, key));
 
     }
+
 }
