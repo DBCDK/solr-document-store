3ebffc7df9d49bcb77e52837e9de242bd804fe34
Morten Bøgeskov <mb@dbc.dk>
2017-12-07 08:47:28 +0100

    Skeleton with Status

---
+++ solr-updater/src/main/java/dk/dbc/search/solrdocstore/updater/rest/Status.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (C) 2017 DBC A/S (http://dbc.dk/)
+ *
+ * This is part of dbc-solr-doc-store-updater
+ *
+ * dbc-solr-doc-store-updater is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * dbc-solr-doc-store-updater is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package dk.dbc.search.solrdocstore.updater.rest;
+
+import javax.ejb.Stateless;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ * @author DBC {@literal <dbc.dk>}
+ */
+@Stateless
+@Path("status")
+public class Status {
+
+    private static final Logger log = LoggerFactory.getLogger(Status.class);
+
+    public Status() {
+    }
+
+    @GET
+    @Produces({MediaType.APPLICATION_JSON})
+    public Response getStatus() {
+        log.info("getStatus called ");
+        return Response.ok(new StatusResponse(true)).build();
+    }
+
+    public static final class StatusResponse {
+
+        public boolean ok;
+
+        public StatusResponse() {
+        }
+
+        public StatusResponse(boolean ok) {
+            this.ok = ok;
+        }
+
+    }
+
+}
