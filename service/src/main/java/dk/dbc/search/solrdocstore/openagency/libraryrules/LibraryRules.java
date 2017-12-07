cc0fb31eddd9effe9268278c19df037305d869c7
Michael Willer <willer@lundogbendsen.dk>
2017-12-07 16:08:15 +0100

    us-2246 Open Agency mock and unittest of LibraryRulesProxy.

--- service/src/main/java/dk/dbc/search/solrdocstore/openagency/libraryrules/LibraryRules.java
+++ service/src/main/java/dk/dbc/search/solrdocstore/openagency/libraryrules/LibraryRules.java
@@ -3,4 +3,10 @@ package dk.dbc.search.solrdocstore.openagency.libraryrules;
 public class LibraryRules {
     String agencyType = null;
     Boolean canUseEnrichments=null;
+
+    public LibraryRules(){}
+    public LibraryRules(String agencyType, boolean canUseEnrichments){
+        this.agencyType = agencyType;
+        this.canUseEnrichments = canUseEnrichments;
+    }
 }
--- service/src/main/java/dk/dbc/search/solrdocstore/openagency/libraryrules/LibraryRulesProxy.java
+++ service/src/main/java/dk/dbc/search/solrdocstore/openagency/libraryrules/LibraryRulesProxy.java
@@ -6,6 +6,7 @@ import net.jodah.failsafe.RetryPolicy;
 
 import javax.ejb.Singleton;
 import javax.ejb.Stateless;
+import javax.inject.Inject;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
@@ -17,17 +18,14 @@ import java.util.concurrent.TimeUnit;
 @Singleton
 public class LibraryRulesProxy {
 
+    @Inject
+    OpenAgencyClient oaclient;
+
     private static String SCHOOLLIBRARY="Skolebibliotek";
 
     public LibraryConfig.LibraryType fetchLibraryTypeFor(int agency){
-        RetryPolicy retryPolicy = new RetryPolicy()
-                .withDelay(100, TimeUnit.MILLISECONDS)
-                .retryOn(Exception.class)
-                .withMaxRetries(4);
 
-        String json = Failsafe.with(retryPolicy).get(() -> fetchJSON(agency));
-
-        LibraryRules libraryRules = JSONParser.getLibraryRules(json);
+        LibraryRules libraryRules = oaclient.fetchLibraryRuleFor(agency);
         if (libraryRules.canUseEnrichments){
             if (SCHOOLLIBRARY.equals(libraryRules.agencyType)){
                 return LibraryConfig.LibraryType.FBSSchool;
@@ -36,25 +34,6 @@ public class LibraryRulesProxy {
             }
         } else {
             return LibraryConfig.LibraryType.NonFBS;
-        }
-    }
-
-
-    private String fetchJSON(int agency){
-
-        Client client=null;
-
-        try {
-            String URI = "http://openagency.addi.dk/next_2.32/?action=libraryRules&outputType=json&agencyId=";
-            client = ClientBuilder.newClient();
-            WebTarget target = client.target(URI + agency);
-            return target.request(MediaType.APPLICATION_JSON).get(String.class);
-        } catch(Exception e){
-            throw new LibraryRuleException("Failed to connect to openagency.addi.dk",e);
-        } finally {
-            if (client!=null) {
-                client.close();
-            }
         }
     }
 
