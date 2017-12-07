1adbec49ee1b95c26ac767306f452684688fc747
Peter Garnæs <pag@dbc.com>
2017-12-07 09:22:08 +0100

    Made index key explorer agnostic of the bibliographic state, made tab layout with related holding tab which will also use the generalised index key explorer.

--- gui/app/components/solr-docstore-gui.js
+++ gui/app/components/solr-docstore-gui.js
@@ -2,18 +2,56 @@ import React from 'react';
 import SearchField from './search_field';
 import ListResults  from './list_results';
 import DisplayError from './display_error';
-import IndexKeyExplorer from './index_key_explorer';
+import BibliographicExplorer from './bibliographic_explorer';
+import RelatedHoldingsExplorer from './related_holdings';
+
+const BIBLIOGRAPHIC_EXPLORER = 0;
+const RELATED_HOLDINGS_EXPLORER = 1;
 
 class SolrDocstoreGUI extends React.PureComponent {
     constructor(props){
         super(props);
         this.state = {
-            pendingSearch: false,
-            searchResults: [],
+            activeItem: BIBLIOGRAPHIC_EXPLORER
         };
+        this.activeTabComponent = this.activeTabComponent.bind(this);
+        this.activeTabWithKey = this.activeTabWithKey.bind(this);
+        this.activateIndexKeyExplorer = this.activateIndexKeyExplorer.bind(this);
+        this.activateRelatedHoldingsExplorer = this.activateRelatedHoldingsExplorer.bind(this);
+        this.isActive = this.isActive.bind(this);
+    }
+
+    activeTabComponent(){
+        switch (this.state.activeItem){
+            case BIBLIOGRAPHIC_EXPLORER:
+                return BibliographicExplorer;
+            case RELATED_HOLDINGS_EXPLORER:
+                return RelatedHoldingsExplorer;
+            default:
+                return BibliographicExplorer;
+        }
+    }
+
+    activeTabWithKey(key){
+        this.setState({
+            activeItem: key
+        })
+    }
+
+    activateIndexKeyExplorer(){
+        this.activeTabWithKey(BIBLIOGRAPHIC_EXPLORER);
+    }
+
+    activateRelatedHoldingsExplorer(){
+        this.activeTabWithKey(RELATED_HOLDINGS_EXPLORER);
+    }
+
+    isActive(key){
+        return (this.state.activeItem === key) ? "active" : "";
     }
 
     render(){
+        let ActiveTab = this.activeTabComponent();
         return (
             <div className="container-full">
                 <div className="row">
@@ -25,7 +63,25 @@ class SolrDocstoreGUI extends React.PureComponent {
                         </div>
                     </div>
                     <div className="col-6">
-                        <IndexKeyExplorer/>
+                        <div className="py-4">
+                            <ul className="nav nav-tabs">
+                                <li className="nav-item">
+                                    <a className={"nav-link "+this.isActive(BIBLIOGRAPHIC_EXPLORER)}
+                                        href="#"
+                                        onClick={this.activateIndexKeyExplorer}>
+                                        Index keys
+                                    </a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className={"nav-link "+this.isActive(RELATED_HOLDINGS_EXPLORER)}
+                                       href="#"
+                                       onClick={this.activateRelatedHoldingsExplorer}>
+                                        Related holdings
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                        <ActiveTab/>
                     </div>
                 </div>
             </div>
