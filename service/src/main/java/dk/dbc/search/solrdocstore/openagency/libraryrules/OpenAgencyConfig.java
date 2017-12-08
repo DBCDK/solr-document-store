82290826590cab256e876f64a6160869d2b3ddc7
Michael Willer <willer@lundogbendsen.dk>
2017-12-08 12:25:46 +0100

    us-2246 forgot a file

---
+++ service/src/main/java/dk/dbc/search/solrdocstore/openagency/libraryrules/OpenAgencyConfig.java
@@ -0,0 +1,64 @@
+package dk.dbc.search.solrdocstore.openagency.libraryrules;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.EJBException;
+import javax.ejb.Startup;
+import javax.ejb.Stateless;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Properties;
+
+
+@Stateless
+@Startup
+public class OpenAgencyConfig {
+
+    private static final Logger log = LoggerFactory.getLogger(OpenAgencyConfig.class);
+
+    private String oaURL;
+
+    @PostConstruct
+    public void loadProperties() {
+        Properties props = findProperties("solr-doc-store-service");
+        oaURL = props.getProperty("openAgencyUrl", System.getenv("OPEN_AGENCY_URL"));
+        if (oaURL==null){
+            throw new EJBException("No URL found for Open Agency. Please provide env:OPEN_AGENCY_URL or property:openAgencyURL");
+        }
+    }
+
+    public String getOaURL() {
+        return oaURL;
+    }
+
+    private Properties findProperties(String resourceName) {
+        try {
+            InputStream resource = this.getClass().getClassLoader().getResourceAsStream(resourceName + ".properties");
+            if (resource!=null){
+                Properties fromFile = new Properties();
+                try {
+                    fromFile.load(resource);
+                    return fromFile;
+                } catch (IOException e){
+                    throw new EJBException("Property file" + resourceName + ".properties exists. But I can't load it?",e);
+                }
+            }
+            Object lookup = InitialContext.doLookup(resourceName);
+            if (lookup instanceof Properties) {
+                return (Properties) lookup;
+            } else {
+                throw new NamingException("Found " + resourceName + ", but not of type Properties of type: " + lookup.getClass().getTypeName());
+            }
+        } catch (NamingException ex) {
+            log.error("Exception: {}", ex.getMessage());
+        }
+        return new Properties();
+    }
+
+}
+
