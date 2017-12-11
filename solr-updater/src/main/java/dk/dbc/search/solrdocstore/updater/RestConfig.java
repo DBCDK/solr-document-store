230ba06d566694fff34560fd3119bc02326ad226
Morten Bøgeskov <mb@dbc.dk>
2017-12-11 09:50:31 +0100

    Solr document producer test endpoint

--- solr-updater/src/main/java/dk/dbc/search/solrdocstore/updater/RestConfig.java
+++ solr-updater/src/main/java/dk/dbc/search/solrdocstore/updater/RestConfig.java
@@ -18,7 +18,9 @@
  */
 package dk.dbc.search.solrdocstore.updater;
 
+import dk.dbc.search.solrdocstore.updater.rest.DocTest;
 import dk.dbc.search.solrdocstore.updater.rest.Status;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
@@ -31,15 +33,12 @@ import javax.ws.rs.core.Application;
  * @author DBC {@literal <dbc.dk>}
  */
 @ApplicationPath("api")
-public class RestConfig extends Application{
-    private static final Set<Class<?>> CLASSES = makeClasses();
+public class RestConfig extends Application {
 
-    private static Set<Class<?>> makeClasses() {
-        HashSet<Class<?>> classes = new HashSet<>();
-
-        classes.add(Status.class);
-        return classes;
-    }
+    private static final Set<Class<?>> CLASSES = new HashSet<>(
+            Arrays.asList(DocProducer.class,
+                          DocTest.class,
+                          Status.class));
 
     @Override
     public Set<Class<?>> getClasses() {
