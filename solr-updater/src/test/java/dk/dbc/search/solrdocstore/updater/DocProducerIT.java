d9027155205ee7dfd708b037a45e75e05d6f4f6e
Morten Bøgeskov <mb@dbc.dk>
2017-12-11 09:50:31 +0100

    Solr document producer test endpoint

---
+++ solr-updater/src/test/java/dk/dbc/search/solrdocstore/updater/DocProducerIT.java
@@ -0,0 +1,250 @@
+/*
+ * Copyright (C) 2017 DBC A/S (http://dbc.dk/)
+ *
+ * This is part of dbc-solr-doc-store-updater
+ *
+ * dbc-solr-doc-store-updater is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * dbc-solr-doc-store-updater is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package dk.dbc.search.solrdocstore.updater;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import dk.dbc.commons.testutils.postgres.connection.PostgresITDataSource;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import javax.sql.DataSource;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import org.apache.solr.client.solrj.SolrClient;
+import org.apache.solr.client.solrj.SolrQuery;
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.response.QueryResponse;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author DBC {@literal <dbc.dk>}
+ */
+public class DocProducerIT {
+
+    private static final Logger log = LoggerFactory.getLogger(DocProducerIT.class);
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+
+    private static PostgresITDataSource pg;
+    private static DataSource dataSource;
+
+    private static String payaraPort;
+    private static String solrDocStoreUrl;
+    private static Payara payara;
+
+    private static String solrPort;
+    private static String solrUrl;
+    private static Solr solr;
+
+    private static Client client;
+
+    private static Config config;
+
+    @ClassRule
+    public static final TemporaryFolder tempSolr = new TemporaryFolder();
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+
+        ExecutorService executor = Executors.newFixedThreadPool(4);
+
+        pg = new PostgresITDataSource("solrdocstore");
+        dataSource = pg.getDataSource();
+
+        List<Future> inits = Arrays.asList(
+                executor.submit(() -> initPayara()),
+                executor.submit(() -> initSolr()));
+
+        client = ClientBuilder.newClient();
+
+        config = new Config() {
+            @Override
+            public String getSolrDocStoreUrl() {
+                return solrDocStoreUrl;
+            }
+
+            @Override
+            public String getSolrUrl() {
+                return solrUrl;
+            }
+
+        };
+
+        inits.stream().forEach((f) -> {
+            try {
+                f.get();
+            } catch (InterruptedException | ExecutionException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+        executor.shutdown();
+        executor.awaitTermination(5, TimeUnit.SECONDS);
+    }
+
+    private static void initPayara() throws RuntimeException {
+        try {
+            payaraPort = System.getProperty("glassfish.port", "18080");
+            solrDocStoreUrl = "http://localhost:" + payaraPort + "/solr-doc-store";
+            payara = new Payara(payaraPort)
+                    .cmd("set-log-level dk.dbc=FINE")
+                    .withDataSource("jdbc/solr-doc-store", pg.getUrl())
+                    .deploy("../service/target/solr-doc-store-service-1.0-SNAPSHOT.war", "/solr-doc-store");
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    private static void initSolr() throws RuntimeException {
+        try {
+            solrPort = System.getProperty("solr.port", "18081");
+            solr = new Solr(solrPort, DocProducerIT.class.getResource("/solr"), tempSolr);
+            solrUrl = "http://localhost:" + solrPort + "/solr/corepo";
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+        solr.stop();
+        payara.stop();
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        try {
+            pg.clearTables("bibliographicSolrKeys", "bibliographictobibliographic", "holdingsitemssolrkeys", "holdingstobibliographic");
+        } catch (SQLException ex) {
+            log.trace("Exception: {}", ex.getMessage());
+        }
+    }
+
+    /*
+     * Test of init method, of class DocProducer.
+     */
+    @Test
+    public void testInit() throws Exception {
+        System.out.println("init");
+        load("test1-part1");
+
+        // FAKE relations
+        try (Connection connection = dataSource.getConnection() ;
+             Statement stmt = connection.createStatement()) {
+            stmt.executeUpdate("DELETE FROM holdingstobibliographic");
+            stmt.executeUpdate("INSERT INTO holdingstobibliographic (holdingsagencyid, bibliographicrecordid, bibliographicagencyid) SELECT agencyid, bibliographicrecordid, 300101 FROM holdingsitemssolrkeys;");
+        }
+        System.out.println(client.target(solrDocStoreUrl + "/api/evict-all").request().get().toString());
+
+        DocProducer docProducer = new DocProducer();
+        docProducer.config = config;
+        docProducer.solrFields = new SolrFields();
+        docProducer.solrFields.config = config;
+        docProducer.solrFields.init();
+
+        docProducer.init();
+
+        SolrClient solrClient = SolrApi.makeSolrClient(solrUrl);
+
+        deployAndSearch(docProducer, solrClient, 3);
+
+        // Merge is no implemented yet, so clear table to load new (deleted) record
+        pg.clearTables("bibliographicSolrKeys", "bibliographictobibliographic", "holdingsitemssolrkeys", "holdingstobibliographic");
+        load("test1-part2");
+        System.out.println(client.target(solrDocStoreUrl + "/api/evict-all").request().get().toString());
+
+        deployAndSearch(docProducer, solrClient, 0);
+
+        System.out.println("OK");
+    }
+
+    private void deployAndSearch(DocProducer docProducer, SolrClient solrClient, int expected) throws SolrServerException, IOException {
+        docProducer.deploy(300101, "23645564", solrClient, 0);
+        solrClient.commit(true, true);
+        QueryResponse response1 = solrClient.query(new SolrQuery("*:*"));
+        System.out.println("response = " + response1);
+        assertEquals(expected, response1.getResults().getNumFound());
+    }
+
+    private void load(String testName) throws IOException {
+        String uuid = UUID.randomUUID().toString();
+        JsonNode rules;
+        try (InputStream is = getClass().getClassLoader().getResourceAsStream("ITRequests/load.json")) {
+            rules = OBJECT_MAPPER.readTree(is);
+        }
+        JsonNode test = rules.get(testName);
+        if (test == null) {
+            throw new IllegalStateException("Don't know about test: " + testName);
+        }
+        for (Iterator<Map.Entry<String, JsonNode>> iterator = test.fields() ; iterator.hasNext() ;) {
+            Map.Entry<String, JsonNode> entry = iterator.next();
+            String api = entry.getKey();
+            JsonNode array = entry.getValue();
+            if (!array.isArray()) {
+                throw new IllegalStateException("Value of " + api + " in test: " + testName + " is not an array");
+            }
+            WebTarget target = client.target(solrDocStoreUrl + "/api/" + api);
+            for (JsonNode file : array) {
+                String fileName = file.asText();
+                log.debug("fileName = {}", fileName);
+                try (InputStream is = getClass().getClassLoader().getResourceAsStream("ITRequests/" + fileName)) {
+                    JsonNode content = OBJECT_MAPPER.readTree(is);
+                    if (content.isObject()) {
+                        ( (ObjectNode) content ).put("trackingId", uuid);
+                    }
+                    Response resp = target.request(MediaType.APPLICATION_JSON_TYPE)
+                            .buildPost(Entity.entity(content.toString(), MediaType.APPLICATION_JSON))
+                            .invoke();
+                    System.out.println("resp.getStatusInfo() = " + resp.getStatusInfo());
+                    if (resp.getStatus() != 200) {
+                        throw new IllegalArgumentException("Cannot post: ITRequests/" + fileName + ": " + resp.getStatusInfo());
+                    }
+                }
+            }
+
+        }
+    }
+
+}
