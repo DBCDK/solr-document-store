f858a29c8796d3768992d7f55e7baa1b7837e4c5
Peter Garnæs <pag@dbc.dk>
2017-12-11 11:34:03 +0100

    Store configuration can now be initialised with an initial state, useful for testing. Basic saga integration testing of related holdings.

--- gui/app/reducers/configure_store.js
+++ gui/app/reducers/configure_store.js
@@ -3,11 +3,12 @@ import rootReducer from "./index";
 import createSagaMiddleware from 'redux-saga';
 import rootSaga from '../sagas';
 
-export default function configureStore() {
+export default function configureStore(initialState = undefined) {
     const sagaMiddleware = createSagaMiddleware();
 
     let store = createStore(
         rootReducer,
+        initialState,
         applyMiddleware(sagaMiddleware)
     );
     sagaMiddleware.run(rootSaga);
