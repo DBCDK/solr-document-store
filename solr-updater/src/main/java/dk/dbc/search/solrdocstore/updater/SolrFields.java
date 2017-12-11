9f608cd21ac91d0ca2566b10f7e72406ec9ecf50
Morten Bøgeskov <mb@dbc.dk>
2017-12-11 07:39:26 +0100

    Extract schema.xml from zookeeper - Run by hand testing

--- solr-updater/src/main/java/dk/dbc/search/solrdocstore/updater/SolrFields.java
+++ solr-updater/src/main/java/dk/dbc/search/solrdocstore/updater/SolrFields.java
@@ -22,7 +22,10 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJBException;
 import javax.ejb.Singleton;
@@ -35,6 +38,12 @@ import javax.ws.rs.core.UriBuilder;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import org.apache.solr.client.solrj.SolrClient;
+import org.apache.solr.client.solrj.impl.CloudSolrClient;
+import org.apache.solr.client.solrj.impl.HttpSolrClient;
+import org.apache.solr.common.cloud.ClusterState;
+import org.apache.solr.common.cloud.DocCollection;
+import org.apache.solr.common.cloud.Replica;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.w3c.dom.Document;
@@ -83,12 +92,45 @@ public class SolrFields {
         processDoc(doc);
     }
 
+    /**
+     * Find an url for any solr client, that should provide a schema.xml
+     *
+     * @return url as string
+     */
     String getUrl() {
-        return config.getSolrUrl();
+        SolrClient client = SolrApi.makeSolrClient(config.getSolrUrl());
+        System.out.println("client.getClass().getCanonicalName() = " + client.getClass().getCanonicalName());
+        if (client instanceof HttpSolrClient) {
+            return config.getSolrUrl();
+        }
+        if (client instanceof CloudSolrClient) {
+            CloudSolrClient cloud = (CloudSolrClient) client;
+
+            String collectionName = cloud.getDefaultCollection();
+            ClusterState state = cloud.getZkStateReader().getClusterState();
+            Set<String> nodes = state.getLiveNodes();
+            if (nodes.isEmpty()) {
+                throw new IllegalArgumentException("Zookeeper don't know about live nodes");
+            }
+
+            List<Replica> liveReplicas = state.getCollection(collectionName)
+                    .getReplicas()
+                    .stream()
+                    .filter(r -> nodes.contains(r.getNodeName()))
+                    .collect(Collectors.toList());
+            if (liveReplicas.isEmpty()) {
+                throw new IllegalArgumentException("Zookeeper don't know any live replicas");
+            }
+
+            Replica replica = liveReplicas.get((int) Math.floor(Math.random() * liveReplicas.size()));
+            System.out.println("replica = " + replica);
+            return replica.getBaseUrl() + "/" + collectionName;
+        }
+        throw new IllegalStateException("Don't know about this solr client type: " + client.getClass().getSimpleName());
     }
 
     /**
-     * Lookup field in knownFields, if nonexistant get a value from
+     * Lookup field in knownFields, if nonexistent get a value from
      * dynamicFieldSpecs
      *
      * @param name name of field
