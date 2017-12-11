903e3e4ff0f4274d6c6de639b8f09cd2c0594e39
Peter Garnæs <pag@dbc.dk>
2017-12-11 10:48:50 +0100

    Removed pull related holdings action, as it is not used.
    Introduced input validation to related holdings reducer so state is
    consistent even with rogue actions not produced by the action producer
    code.
    
    Added testing for invalid related holdings reducer input

--- gui/app/actions/related_holdings.js
+++ gui/app/actions/related_holdings.js
@@ -1,12 +1,5 @@
-export const PULL_RELATED_HOLDINGS = 'PULL_RELATED_HOLDINGS';
 export const PULL_RELATED_HOLDINGS_SUCCESS = 'PULL_RELATED_HOLDINGS_SUCCESS';
 export const PULL_RELATED_HOLDINGS_FAILED = 'PULL_RELATED_HOLDINGS_FAILED';
-
-export const pullRelatedHoldings = (bibliographicRecordId,bibliographicAgencyId) => ({
-    type: PULL_RELATED_HOLDINGS,
-    bibliographicRecordId,
-    bibliographicAgencyId
-});
 
 export const pullSuccess = (result) => ({
     type: PULL_RELATED_HOLDINGS_SUCCESS,
--- gui/app/reducers/related_holdings.js
+++ gui/app/reducers/related_holdings.js
@@ -16,26 +16,39 @@ const initialState = {
 export default function relatedHoldings(state = initialState, action = {}) {
     switch (action.type){
         case SELECT_BIB_RECORD:
+            // handling invalid actions
+            let validAction = !!(action.item) &&
+                (action.item.bibliographicRecordId !== undefined) &&
+                (action.item.agencyId !== undefined);
+            action.item = (action.item) ? action.item : {
+                bibliographicRecordId: null,
+                agencyId: null
+            };
+            let actionBibId = action.item.bibliographicRecordId;
+            let actionAgencyId = action.item.agencyId;
+            // If values are undefined, set to null instead
+            let bibRecordId = (validAction && actionBibId) ? actionBibId : null;
+            let agencyId = (validAction && actionAgencyId) ? actionAgencyId : null;
             return update(state,{
-                loading: {$set: true},
+                loading: {$set: validAction},
                 errorMessage: {$set: ''},
-                selectedBibRecordId: {$set: action.item.bibliographicRecordId},
-                selectedBibAgencyId: {$set: action.item.agencyId}
-            });
-        case PULL_RELATED_HOLDINGS:
-            return update(state,{
-                loading: {$set: true}
+                selectedBibRecordId: {$set: bibRecordId},
+                selectedBibAgencyId: {$set: agencyId}
             });
         case PULL_RELATED_HOLDINGS_SUCCESS:
+            // Handling invalid actions
+            let result = (action.result && Array.isArray(action.result)) ? action.result : [];
             return update(state,{
                 loading: {$set: false},
                 errorMessage: {$set: ''},
-                relatedHoldings: {$set: action.result},
+                relatedHoldings: {$set: result},
             });
         case PULL_RELATED_HOLDINGS_FAILED:
+            // Handling invalid action, will ignored
+            let message = (action.message && (typeof action.message === 'string')) ? action.message : '';
             return update(state,{
                 loading: {$set: false},
-                errorMessage: {$set: action.message}
+                errorMessage: {$set: message}
             });
         default:
             return state;
