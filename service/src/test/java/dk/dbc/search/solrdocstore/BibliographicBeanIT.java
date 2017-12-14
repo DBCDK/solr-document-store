766e17168a860a6443c10b95fa56f1d6dd4acf5b
Morten Bøgeskov <mb@dbc.dk>
2017-12-14 09:33:00 +0100

    Integration test of B2B superceds

--- service/src/test/java/dk/dbc/search/solrdocstore/BibliographicBeanIT.java
+++ service/src/test/java/dk/dbc/search/solrdocstore/BibliographicBeanIT.java
@@ -2,16 +2,21 @@ package dk.dbc.search.solrdocstore;
 
 import dk.dbc.commons.jsonb.JSONBContext;
 import dk.dbc.commons.jsonb.JSONBException;
+import java.util.Arrays;
+
 import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
+
 import org.junit.Before;
 import org.junit.Test;
 
 import javax.persistence.EntityManager;
 import javax.ws.rs.core.Response;
 import java.util.List;
+import java.util.function.Consumer;
+import org.junit.Assert;
 
 public class BibliographicBeanIT extends JpaSolrDocStoreIntegrationTester {
 
@@ -28,12 +33,15 @@ public class BibliographicBeanIT extends JpaSolrDocStoreIntegrationTester {
         bean.libraryConfig = new LibraryConfig() {
             @Override
             public LibraryType getLibraryType(int agencyId) {
-                if (agencyId >= 800000) return LibraryType.NonFBS;
-                if (agencyId < 400000) return LibraryType.FBSSchool;
+                if (agencyId >= 800000) {
+                    return LibraryType.NonFBS;
+                }
+                if (agencyId < 400000) {
+                    return LibraryType.FBSSchool;
+                }
                 return LibraryType.FBS;
             }
         };
-
 
         executeScriptResource("/bibliographicUpdate.sql");
     }
@@ -41,7 +49,7 @@ public class BibliographicBeanIT extends JpaSolrDocStoreIntegrationTester {
     @Test
     public void newCommonRecordWithExistingHoldings() throws Exception {
 
-        String b870970 = getBibliographicRequestJson(870970);
+        String b870970 = makeBibliographicRequestJson(870970);
 
         Response r = env().getPersistenceContext()
                 .run(() -> bean.addBibliographicKeys(null, b870970)
@@ -52,13 +60,13 @@ public class BibliographicBeanIT extends JpaSolrDocStoreIntegrationTester {
         List<HoldingsToBibliographicEntity> l = em.createQuery("SELECT h FROM HoldingsToBibliographicEntity as h WHERE h.bibliographicRecordId='new'", HoldingsToBibliographicEntity.class).getResultList();
 
         assertThat(l, containsInAnyOrder(
-                new HoldingsToBibliographicEntity(700000, "new", 870970),
-                new HoldingsToBibliographicEntity(700100, "new", 870970),
-                new HoldingsToBibliographicEntity(300100, "new", 870970),
-                new HoldingsToBibliographicEntity(300200, "new", 870970)
-        ));
+                   new HoldingsToBibliographicEntity(700000, "new", 870970),
+                   new HoldingsToBibliographicEntity(700100, "new", 870970),
+                   new HoldingsToBibliographicEntity(300100, "new", 870970),
+                   new HoldingsToBibliographicEntity(300200, "new", 870970)
+           ));
 
-        String b700000 = getBibliographicRequestJson(700000);
+        String b700000 = makeBibliographicRequestJson(700000);
 
         r = env().getPersistenceContext()
                 .run(() -> bean.addBibliographicKeys(null, b700000)
@@ -69,14 +77,13 @@ public class BibliographicBeanIT extends JpaSolrDocStoreIntegrationTester {
         l = em.createQuery("SELECT h FROM HoldingsToBibliographicEntity as h WHERE h.bibliographicRecordId='new'", HoldingsToBibliographicEntity.class).getResultList();
 
         assertThat(l, containsInAnyOrder(
-                new HoldingsToBibliographicEntity(700000, "new", 700000),
-                new HoldingsToBibliographicEntity(700100, "new", 870970),
-                new HoldingsToBibliographicEntity(300100, "new", 870970),
-                new HoldingsToBibliographicEntity(300200, "new", 870970)
-        ));
+                   new HoldingsToBibliographicEntity(700000, "new", 700000),
+                   new HoldingsToBibliographicEntity(700100, "new", 870970),
+                   new HoldingsToBibliographicEntity(300100, "new", 870970),
+                   new HoldingsToBibliographicEntity(300200, "new", 870970)
+           ));
 
-
-        String b300100 = getBibliographicRequestJson(300100);
+        String b300100 = makeBibliographicRequestJson(300100);
 
         r = env().getPersistenceContext()
                 .run(() -> bean.addBibliographicKeys(null, b300100)
@@ -85,34 +92,31 @@ public class BibliographicBeanIT extends JpaSolrDocStoreIntegrationTester {
 
         l = em.createQuery("SELECT h FROM HoldingsToBibliographicEntity as h WHERE h.bibliographicRecordId='new'", HoldingsToBibliographicEntity.class).getResultList();
         assertThat(l, containsInAnyOrder(
-                new HoldingsToBibliographicEntity(700000, "new", 700000),
-                new HoldingsToBibliographicEntity(700100, "new", 870970),
-                new HoldingsToBibliographicEntity(300100, "new", 300100),
-                new HoldingsToBibliographicEntity(300200, "new", 870970)
-        ));
+                   new HoldingsToBibliographicEntity(700000, "new", 700000),
+                   new HoldingsToBibliographicEntity(700100, "new", 870970),
+                   new HoldingsToBibliographicEntity(300100, "new", 300100),
+                   new HoldingsToBibliographicEntity(300200, "new", 870970)
+           ));
 
-
-        String b300000 = getBibliographicRequestJson(300000);
+        String b300000 = makeBibliographicRequestJson(300000);
 
         r = env().getPersistenceContext()
                 .run(() -> bean.addBibliographicKeys(null, b300000)
                 );
         assertThat(r.getStatus(), is(200));
 
-
         l = em.createQuery("SELECT h FROM HoldingsToBibliographicEntity as h WHERE h.bibliographicRecordId='new'", HoldingsToBibliographicEntity.class).getResultList();
         assertThat(l, containsInAnyOrder(
-                new HoldingsToBibliographicEntity(700000, "new", 700000),
-                new HoldingsToBibliographicEntity(700100, "new", 870970),
-                new HoldingsToBibliographicEntity(300100, "new", 300100),
-                new HoldingsToBibliographicEntity(300200, "new", 300000)
-        ));
+                   new HoldingsToBibliographicEntity(700000, "new", 700000),
+                   new HoldingsToBibliographicEntity(700100, "new", 870970),
+                   new HoldingsToBibliographicEntity(300100, "new", 300100),
+                   new HoldingsToBibliographicEntity(300200, "new", 300000)
+           ));
     }
-
 
     @Test
     public void updateNonFbsLibrary() throws JSONBException {
-        String json = getBibliographicRequestJson(800000);
+        String json = makeBibliographicRequestJson(800000);
 
         Response r = env().getPersistenceContext()
                 .run(() -> bean.addBibliographicKeys(null, json)
@@ -122,12 +126,12 @@ public class BibliographicBeanIT extends JpaSolrDocStoreIntegrationTester {
 
         List<HoldingsToBibliographicEntity> l = em.createQuery("SELECT h FROM HoldingsToBibliographicEntity as h WHERE h.bibliographicRecordId='new'", HoldingsToBibliographicEntity.class).getResultList();
 
-        assertThat(l, contains((new HoldingsToBibliographicEntity(800000, "new", 800000))));
+        assertThat(l, contains(( new HoldingsToBibliographicEntity(800000, "new", 800000) )));
     }
 
     @Test
     public void updateNonFbsLibraryWithNoHolding() throws JSONBException {
-        String json = getBibliographicRequestJson(888000);
+        String json = makeBibliographicRequestJson(888000);
 
         Response r = env().getPersistenceContext()
                 .run(() -> bean.addBibliographicKeys(null, json)
@@ -140,14 +144,39 @@ public class BibliographicBeanIT extends JpaSolrDocStoreIntegrationTester {
         assertThat(l.size(), is(0));
     }
 
+    @Test
+    public void supercedsAdd() throws Exception {
 
-    private String getBibliographicRequestJson(int agency) throws JSONBException {
-        BibliographicEntity entity = new BibliographicEntity();
-        entity.agencyId = agency;
-        entity.bibliographicRecordId = "new";
-        entity.unit = "u";
-        entity.work = "w";
-        entity.trackingId = "IT";
+        String json = makeBibliographicRequestJson(888000,
+                                                   e -> {
+                                                       e.superceds = Arrays.asList("a", "b");
+                                                   });
+
+        Response r = env().getPersistenceContext()
+                .run(() -> bean.addBibliographicKeys(null, json)
+                );
+        assertThat(r.getStatus(), is(200));
+        List<BibliographicToBibliographicEntity> l = em.createQuery("SELECT b2b FROM BibliographicToBibliographicEntity as b2b WHERE b2b.currentRecordId='new'", BibliographicToBibliographicEntity.class).getResultList();
+
+        assertThat(l.size(), is(2));
+        Assert.assertTrue("One superceded named 'a'", l.stream().anyMatch(b2b -> b2b.decommissionedRecordId.equals("a")));
+        Assert.assertTrue("One superceded named 'b'", l.stream().anyMatch(b2b -> b2b.decommissionedRecordId.equals("b")));
+    }
+
+    private String makeBibliographicRequestJson(int agency) throws JSONBException {
+        return makeBibliographicRequestJson(agency, e -> {
+                                    });
+    }
+
+    private String makeBibliographicRequestJson(int agency, Consumer<BibliographicEntityRequest> modifier) throws JSONBException {
+        BibliographicEntityRequest entity1 = new BibliographicEntityRequest();
+        entity1.agencyId = agency;
+        entity1.bibliographicRecordId = "new";
+        entity1.unit = "u";
+        entity1.work = "w";
+        entity1.trackingId = "IT";
+        BibliographicEntityRequest entity = entity1;
+        modifier.accept(entity);
         return jsonbContext.marshall(entity);
     }
 
