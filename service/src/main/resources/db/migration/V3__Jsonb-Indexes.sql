344e0eb7161cbd9070e1a5a35aa3c914249b5332
Morten Bøgeskov <mb@dbc.dk>
2018-01-05 13:37:18 +0100

    JsonB GIN Indexes (NULL safe)

--- service/src/main/resources/db/migration/V3__Jsonb-Indexes.sql
+++ service/src/main/resources/db/migration/V3__Jsonb-Indexes.sql
@@ -1,2 +1,2 @@
-CREATE INDEX bibliographicSolrKeysJson on bibliographicSolrKeys USING GIN (indexKeys jsonb_path_ops);
-CREATE INDEX holdingsItemsSolrKeysJson on holdingsItemsSolrKeys USING GIN (indexKeys jsonb_path_ops);
+CREATE INDEX bibliographicSolrKeysJson on bibliographicSolrKeys USING GIN (indexKeys jsonb_path_ops) WHERE indexKeys IS NOT NULL;
+CREATE INDEX holdingsItemsSolrKeysJson on holdingsItemsSolrKeys USING GIN (indexKeys jsonb_path_ops) WHERE indexKeys IS NOT NULL;
