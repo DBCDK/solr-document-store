5cedf1b70ac892baf9a1643a340e5981d899ee90
Morten Bøgeskov <mb@dbc.dk>
2018-01-05 12:30:41 +0100

    QueueAll methods on QueueRulesBean

--- service/src/main/java/dk/dbc/search/solrdocstore/QueueRulesBean.java
+++ service/src/main/java/dk/dbc/search/solrdocstore/QueueRulesBean.java
@@ -18,11 +18,19 @@
  */
 package dk.dbc.search.solrdocstore;
 
+import dk.dbc.search.solrdocstore.queue.QueueJob;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Arrays;
 import java.util.List;
+import javax.annotation.Resource;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
+import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -35,8 +43,13 @@ public class QueueRulesBean {
 
     private static final Logger log = LoggerFactory.getLogger(QueueRulesBean.class);
 
+    @Resource(lookup = "jdbc/solr-doc-store")
+    DataSource dataSource;
+
     @PersistenceContext(unitName = "solrDocumentStore_PU")
     EntityManager entityManager;
+
+    int commitEvery = 1000;
 
     public List<QueueRuleEntity> getAllQueueRules() {
         TypedQuery<QueueRuleEntity> query = entityManager.createQuery("SELECT qr FROM QueueRuleEntity qr",
@@ -55,4 +68,47 @@ public class QueueRulesBean {
     public void delQueueRule(QueueRuleEntity entity) {
         entityManager.remove(entity);
     }
+
+    public void queueNotDeletedManifestations(String queue) throws SQLException {
+        try (Connection connection = dataSource.getConnection() ;
+             Statement stmt = connection.createStatement() ;
+             ResultSet resultSet = stmt.executeQuery("SELECT agencyId, bibliographicRecordId FROM bibliographicsolrkeys WHERE NOT deleted")) {
+            enqueueManifestations(connection, resultSet, queue);
+        }
+    }
+
+    public void queueAllManifestations(String queue) throws SQLException {
+        try (Connection connection = dataSource.getConnection() ;
+             Statement stmt = connection.createStatement() ;
+             ResultSet resultSet = stmt.executeQuery("SELECT agencyId, bibliographicRecordId FROM bibliographicsolrkeys")) {
+            enqueueManifestations(connection, resultSet, queue);
+        }
+    }
+
+    private void enqueueManifestations(Connection connection, ResultSet resultSet, String queue) throws SQLException {
+        connection.setAutoCommit(false);
+        EnqeueService<AgencyItemKey> enqueueService = getManifestationEnqueueService(connection, queue);
+        long row = 0;
+        while (resultSet.next()) {
+            int i = 0;
+            int agencyId = resultSet.getInt(++i);
+            String bibliographicRecordId = resultSet.getString(++i);
+            enqueueService.enqueue(new AgencyItemKey(agencyId, bibliographicRecordId));
+            if (++row % commitEvery == 0) {
+                connection.commit();
+            }
+        }
+        if (++row % commitEvery != 0) {
+            connection.commit();
+        }
+    }
+
+    private EnqeueService<AgencyItemKey> getManifestationEnqueueService(Connection connection, String queue) {
+        return new EnqeueService<>(connection, Arrays.asList(queue),
+                                   (key, commitWithin) ->
+                                   new QueueJob(key.agencyId,
+                                                key.bibliographicRecordId,
+                                                commitWithin));
+    }
+
 }
