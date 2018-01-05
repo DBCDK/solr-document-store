c8d7f0d6eac85fa6117aecc943f4b760df079f18
Morten Bøgeskov <mb@dbc.dk>
2018-01-05 13:46:22 +0100

    JsonB GIN Indexes Concurrently

--- service/src/main/resources/db/migration/V3__Jsonb-Indexes.sql
+++ service/src/main/resources/db/migration/V3__Jsonb-Indexes.sql
@@ -1,2 +1,2 @@
-CREATE INDEX bibliographicSolrKeysJson on bibliographicSolrKeys USING GIN (indexKeys jsonb_path_ops) WHERE indexKeys IS NOT NULL;
-CREATE INDEX holdingsItemsSolrKeysJson on holdingsItemsSolrKeys USING GIN (indexKeys jsonb_path_ops) WHERE indexKeys IS NOT NULL;
+CREATE INDEX CONCURRENTLY bibliographicSolrKeysJson on bibliographicSolrKeys USING GIN (indexKeys jsonb_path_ops) WHERE indexKeys IS NOT NULL;
+CREATE INDEX CONCURRENTLY holdingsItemsSolrKeysJson on holdingsItemsSolrKeys USING GIN (indexKeys jsonb_path_ops) WHERE indexKeys IS NOT NULL;
