83cf7b07e40b487bdd66668fa34f3a2827ff3cf4
Morten Bøgeskov <mb@dbc.dk>
2018-01-05 10:26:32 +0100

    QueueRule access bean

---
+++ service/src/main/java/dk/dbc/search/solrdocstore/QueueRulesBean.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (C) 2018 DBC A/S (http://dbc.dk/)
+ *
+ * This is part of dbc-solr-doc-store-service
+ *
+ * dbc-solr-doc-store-service is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * dbc-solr-doc-store-service is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package dk.dbc.search.solrdocstore;
+
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ * @author DBC {@literal <dbc.dk>}
+ */
+@Stateless
+public class QueueRulesBean {
+
+    private static final Logger log = LoggerFactory.getLogger(QueueRulesBean.class);
+
+    @PersistenceContext(unitName = "solrDocumentStore_PU")
+    EntityManager entityManager;
+
+    public List<QueueRuleEntity> getAllQueueRules() {
+        TypedQuery<QueueRuleEntity> query = entityManager.createQuery("SELECT qr FROM QueueRuleEntity qr",
+                                                                      QueueRuleEntity.class);
+        return query.getResultList();
+    }
+
+    public QueueRuleEntity getQueueRule(String id) {
+        return entityManager.find(QueueRuleEntity.class, id);
+    }
+
+    public void setQueueRule(QueueRuleEntity entity) {
+        entityManager.merge(entity);
+    }
+
+    public void delQueueRule(QueueRuleEntity entity) {
+        entityManager.remove(entity);
+    }
+}
---
+++ service/src/test/java/dk/dbc/search/solrdocstore/QueueRulesBeanIT.java
@@ -0,0 +1,127 @@
+/*
+ * Copyright (C) 2018 DBC A/S (http://dbc.dk/)
+ *
+ * This is part of dbc-solr-doc-store-service
+ *
+ * dbc-solr-doc-store-service is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * dbc-solr-doc-store-service is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package dk.dbc.search.solrdocstore;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+import java.util.concurrent.Executors;
+import javax.persistence.EntityManager;
+import javax.sql.DataSource;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author DBC {@literal <dbc.dk>}
+ */
+public class QueueRulesBeanIT extends JpaSolrDocStoreIntegrationTester {
+
+    private QueueRulesBean bean;
+    private QueueRulesDaemon daemon;
+    private EntityManager em;
+
+    public QueueRulesBeanIT() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() throws SQLException {
+        DataSource datasource = jpaTestEnvironment.getDatasource();
+        new DatabaseMigrator(datasource).migrate();
+        try (Connection connection = datasource.getConnection() ;
+             Statement stmt = connection.createStatement()) {
+            stmt.executeUpdate("TRUNCATE queueRule");
+        }
+
+        em = env().getEntityManager();
+
+        daemon = new QueueRulesDaemon();
+        daemon.dataSource = datasource;
+        daemon.mes = Executors.newCachedThreadPool();
+        daemon.init();
+
+        bean = new QueueRulesBean();
+        bean.entityManager = em;
+
+    }
+
+    @After
+    public void tearDown() {
+        daemon.destroy();
+    }
+
+    @Test
+    public void persist() throws Exception {
+        System.out.println("persist");
+        assertThat("queuerule table is empty", getAllQueueRules().isEmpty(), is(true));
+        assertNull("Non existing queue foo", getFoo());
+
+        System.out.println(" - set foo - DO");
+        env().getPersistenceContext().run(() -> bean.setQueueRule(new QueueRuleEntity("foo")));
+        System.out.println(" - set foo - DONE");
+        assertNotNull("queuerule for foo exists", getFoo());
+        System.out.println("getAllQueueRules() = " + getAllQueueRules());
+        assertThat("queuerule table is no longer empty", getAllQueueRules().isEmpty(), is(false));
+        waitForQueuesEmptyIs(false);
+        assertThat("Daemon has heard about the new queue", daemon.getManifestationQueues(), contains("foo"));
+
+        System.out.println(" - del foo - DO");
+        QueueRuleEntity foo = getFoo();
+        env().getPersistenceContext().run(() -> bean.delQueueRule(foo));
+        System.out.println(" - del foo - DONE");
+        assertNull("Non existing queue foo", getFoo());
+        assertThat("queuerule table is again empty", getAllQueueRules().isEmpty(), is(true));
+        waitForQueuesEmptyIs(true);
+        assertThat("Daemon has no longer knows about the queue", daemon.getManifestationQueues().isEmpty(), is(true));
+    }
+
+    public void waitForQueuesEmptyIs(boolean isEmpty) throws InterruptedException {
+        for (int i = 0 ; i < 1000 ; i++) {
+            if (daemon.getManifestationQueues().isEmpty() == isEmpty) {
+                break;
+            }
+            Thread.sleep(1L);
+        }
+    }
+
+    private QueueRuleEntity getFoo() {
+        return env().getPersistenceContext().run(() -> bean.getQueueRule("foo"));
+    }
+
+    private List<QueueRuleEntity> getAllQueueRules() {
+        return env().getPersistenceContext().run(bean::getAllQueueRules);
+    }
+
+}
