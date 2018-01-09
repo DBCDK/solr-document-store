9e7db68780e6fdb467db23fa2b62e52ab40f6824
Morten Bøgeskov <mb@dbc.dk>
2018-01-09 14:39:58 +0100

    Use non-transactional-connection for flyway database version handling

--- service/src/main/docker/config.d/01_setup_db_env.sh
+++ service/src/main/docker/config.d/01_setup_db_env.sh
@@ -1,4 +1,2 @@
-#!/usr/bin/env bash
-
-# Function to get local local scope for name
 add_jdbc_resource_from_url "jdbc/solr-doc-store" ${DOCSTORE_POSTGRES_URL}
+add_jdbc_resource_from_url "jdbc/solr-doc-store-flyway" ${DOCSTORE_POSTGRES_URL} 'non-transactional-connections="true"'
