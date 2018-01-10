0ac91ce25fe19f16497271fec85e33fc054cc4d3
Morten Bøgeskov <mb@dbc.dk>
2018-01-10 07:24:57 +0100

    Rename non transactional connection to -nt (from -flyway)

--- service/src/main/docker/config.d/01_setup_db_env.sh
+++ service/src/main/docker/config.d/01_setup_db_env.sh
@@ -1,2 +1,2 @@
 add_jdbc_resource_from_url "jdbc/solr-doc-store" ${DOCSTORE_POSTGRES_URL}
-add_jdbc_resource_from_url "jdbc/solr-doc-store-flyway" ${DOCSTORE_POSTGRES_URL} 'non-transactional-connections="true"'
+add_jdbc_resource_from_url "jdbc/solr-doc-store-nt" ${DOCSTORE_POSTGRES_URL} 'non-transactional-connections="true"'
