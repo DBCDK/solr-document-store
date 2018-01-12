09af8b83b1927e829f65612db02a0ddc9d4aa043
Morten Bøgeskov <mb@dbc.dk>
2018-01-12 10:32:20 +0100

    Create index if database is empty, and schema doesn't already exist

--- service/src/main/resources/db/migration/V3__Jsonb-Indexes.sql
+++ service/src/main/resources/db/migration/V3__Jsonb-Indexes.sql
@@ -1,2 +1,41 @@
-CREATE INDEX CONCURRENTLY bibliographicSolrKeysJson on bibliographicSolrKeys USING GIN (indexKeys jsonb_path_ops) WHERE indexKeys IS NOT NULL;
-CREATE INDEX CONCURRENTLY holdingsItemsSolrKeysJson on holdingsItemsSolrKeys USING GIN (indexKeys jsonb_path_ops) WHERE indexKeys IS NOT NULL;
+
+DO
+$$
+DECLARE
+  has BOOLEAN;
+BEGIN
+    SELECT true INTO has FROM pg_indexes
+        WHERE tablename='bibliographicsolrkeys' AND
+              indexname='bibliographicsolrkeysjson';
+    IF has IS NULL THEN
+        -- RAISE NOTICE 'index "bibliographicsolrkeysjson" is unknown';
+        SELECT true INTO has FROM bibliographicSolrKeys LIMIT 1;
+        IF has = true THEN
+            RAISE NOTICE 'table "bibliographicsolrkeys" has content';
+        ELSE
+            EXECUTE 'CREATE INDEX bibliographicsolrkeysjson on bibliographicsolrkeys USING GIN (indexkeys jsonb_path_ops) WHERE indexkeys IS NOT NULL';
+        END IF;
+    END IF;
+END
+$$;
+
+DO
+$$
+DECLARE
+  has BOOLEAN;
+BEGIN
+    SELECT true INTO has FROM pg_indexes
+        WHERE tablename='holdingsitemssolrkeys' AND
+              indexname='holdingsitemssolrkeysjson';
+    IF has IS NULL THEN
+        -- RAISE NOTICE 'index "holdingsitemssolrkeysjson" is unknown';
+        SELECT true INTO has FROM holdingsitemsSolrKeys LIMIT 1;
+        IF has = true THEN
+            RAISE NOTICE 'table "holdingsitemssolrkeys" has content';
+        ELSE
+            EXECUTE 'CREATE INDEX holdingsitemssolrkeysjson on holdingsitemssolrkeys USING GIN (indexkeys jsonb_path_ops) WHERE indexkeys IS NOT NULL';
+        END IF;
+    END IF;
+END
+$$;
+
