625c01153da47e7b6ad26fcfcdd8b0fedec0043c
Peter Garnæs <pag@dbc.dk>
2018-01-16 10:49:15 +0100

    New bundle setup to support code split build and development environmentt of queue tool

---
+++ gui/app/queue-admin.js
@@ -0,0 +1,35 @@
+import regeneratorRuntime from "regenerator-runtime/runtime";
+const React = require("react");
+const ReactDOM = require("react-dom");
+import { AppContainer } from "react-hot-loader";
+// Main app
+import QueueAdminGUI from "./components/queue-admin-gui";
+// Redux related
+import { Provider } from "react-redux";
+import configureStore from "./reducers/configure_store";
+
+// Webpack will bundle styling
+import "bootstrap/dist/css/bootstrap.min.css";
+import "font-awesome/css/font-awesome.min.css";
+
+let store = configureStore();
+
+const render = Component => {
+  ReactDOM.render(
+      <AppContainer>
+        <Provider store={store}>
+          <Component />
+        </Provider>
+      </AppContainer>,
+      document.getElementById("queue-admin-gui-root")
+  );
+};
+
+render(QueueAdminGUI);
+
+// Webpack Hot Module Replacement API
+if (module.hot) {
+  module.hot.accept(() => {
+    render(QueueAdminGUI);
+  });
+}
