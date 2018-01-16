a9e9b4cff6f21b0ab26095a1af4b5ddf7b8cf5e3
Peter Garnæs <pag@dbc.com>
2018-01-16 10:49:15 +0100

    New bundle setup to support code split build and development environmentt of queue tool

---
+++ gui/app/components/queue-admin-gui.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class QueueAdminGUI extends React.PureComponent {
+  render(){
+    return (
+        <div>Testing</div>
+    )
+  }
+}
+
+export default QueueAdminGUI;
