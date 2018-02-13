8c81dd28030815991187e0f9b889fcbea2a8ca68
Peter Garnæs <pag@dbc.dk>
2018-02-13 09:57:41 +0100

    Sorting in index key explorer now slightly more sophisticated, puts expandable elements last, sorting each group alphabetically

--- gui/app/components/index_key_explorer/index.js
+++ gui/app/components/index_key_explorer/index.js
@@ -1,6 +1,12 @@
 import React from "react";
 import Parent from "./parent";
 
+let keySort = (k1, k2) => {
+  if (k1 === k2) return 0;
+  else if (k1 > k2) return 1;
+  else return -1;
+};
+
 const IndexKeyExplorer = ({
   item,
   HeaderComponentClass,
@@ -9,7 +15,15 @@ const IndexKeyExplorer = ({
   defaultExpansion = false
 }) => {
   return Object.keys(item)
-    .sort()
+    .sort((k1, k2) => {
+      let k1IsParentElement = item[k1] instanceof Array;
+      let k2IsParentElement = item[k2] instanceof Array;
+      if (k1IsParentElement === k2IsParentElement) {
+        return keySort(k1, k2);
+      } else {
+        return k1IsParentElement < k2IsParentElement;
+      }
+    })
     .map(
       key =>
         item[key] instanceof Array ? (
