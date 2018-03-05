ff9849b7b3c2790572547a1c34aa633cf87627ac
Morten Bøgeskov <mb@dbc.dk>
2018-03-05 07:28:04 +0100

    Interceptor based monitoring (Timed/Metered from codahale metrics)

---
+++ service/src/main/java/dk/dbc/search/solrdocstore/monitor/MeteredInterceptor.java
@@ -0,0 +1,43 @@
+package dk.dbc.search.solrdocstore.monitor;
+
+import com.codahale.metrics.Meter;
+import com.codahale.metrics.MetricRegistry;
+import java.lang.reflect.Method;
+import java.util.concurrent.ConcurrentHashMap;
+import javax.inject.Inject;
+import javax.interceptor.AroundInvoke;
+import javax.interceptor.Interceptor;
+import javax.interceptor.InvocationContext;
+
+/**
+ *
+ * @author DBC {@literal <dbc.dk>}
+ */
+@Metered
+@Interceptor
+public class MeteredInterceptor {
+
+    private static final ConcurrentHashMap<Method, Meter> METERS = new ConcurrentHashMap<>();
+
+    @Inject
+    JmxMetrics metrics;
+
+    @AroundInvoke
+    public Object timer(InvocationContext ic) throws Exception {
+        Method method = ic.getMethod();
+        Meter meter = METERS.computeIfAbsent(method, this::makeMeter);
+        meter.mark();
+        return ic.proceed();
+    }
+
+    private Meter makeMeter(Method method) {
+        String methodName = method.getName();
+        Metered metered = method.getAnnotation(Metered.class);
+        if (metered != null && !metered.value().isEmpty()) {
+            methodName = metered.value();
+        }
+        String name = MetricRegistry.name(method.getDeclaringClass(), methodName);
+        return metrics.getRegistry().meter(name);
+    }
+
+}
