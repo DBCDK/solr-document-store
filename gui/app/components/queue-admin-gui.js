030d839be38305360e6b5f1301e71816e8e83b58
Peter Garnæs <pag@dbc.dk>
2018-03-13 12:48:28 +0100

    Added system name on user interface

--- gui/app/components/queue-admin-gui.js
+++ gui/app/components/queue-admin-gui.js
@@ -4,6 +4,22 @@ import AsyncJobMonitor from "./async_job_monitor";
 import EnqueueAsyncJobCreator from "./enqueue-async-job-creator";
 
 class QueueAdminGUI extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      systemName: ""
+    };
+  }
+
+  componentDidMount() {
+    // Small script to figure out what system we are running on, since this is a
+    // pretty simple procedure we skip the whole redux thing
+    fetch("/api/status/system")
+      .then(res => res.json())
+      .then(json => this.setState({ systemName: json.systemName }))
+      .catch(e => {});
+  }
+
   render() {
     return (
       <div>
@@ -11,6 +27,7 @@ class QueueAdminGUI extends React.PureComponent {
           <h3 className="navbar-brand text-light">
             solr-document-store Kø-værktøj
           </h3>
+          <h4 className="text-light mx-4">{this.state.systemName}</h4>
           <a className="nav-link text-light" href="/">
             Søge værktøj
           </a>
