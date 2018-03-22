ea329cf8b30156719c621688ebacd5851de5a611
Peter Garnæs <pag@dbc.dk>
2018-03-22 14:53:01 +0100

    Made input fields look nicer in async job creation components

--- gui/app/components/enqueue-async-job-creator/enqueue_all.js
+++ gui/app/components/enqueue-async-job-creator/enqueue_all.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { connect } from "react-redux";
 import { enqueueAllJob } from "../../actions/async_job";
-import { Button } from "reactstrap";
+import { Button, Input } from "reactstrap";
 
 class EnqueueAllAsyncJob extends React.Component {
   constructor(props) {
@@ -18,27 +18,30 @@ class EnqueueAllAsyncJob extends React.Component {
     return (
       <div className="py-3">
         <h5>Sæt alle poster i kø</h5>
-        <input
-          placeholder="Kø navn"
-          value={this.state.queueInput}
-          onChange={e => this.setState({ queueInput: e.target.value })}
-        />
-        <span
-          className="px-4"
-          style={{ cursor: "pointer" }}
-          onClick={this.toggleDeleteIncluded}
-        >
-          Slettede poster inkluderet:
-          <input
-            type="checkbox"
-            aria-label="Checkbox for following text input"
-            checked={this.state.includeDeleted}
-            onClick={this.toggleDeleteIncluded}
+        <div className="d-flex">
+          <Input
+            placeholder="Kø navn"
+            style={{ width: "15vw" }}
+            value={this.state.queueInput}
+            onChange={e => this.setState({ queueInput: e.target.value })}
           />
-        </span>
-        <Button color="primary" onClick={this.enqueueAll}>
-          Start job
-        </Button>
+          <span
+            className="px-4"
+            style={{ cursor: "pointer" }}
+            onClick={this.toggleDeleteIncluded}
+          >
+            Slettede poster inkluderet:
+            <input
+              type="checkbox"
+              aria-label="Checkbox for following text input"
+              checked={this.state.includeDeleted}
+              onClick={this.toggleDeleteIncluded}
+            />
+          </span>
+          <Button color="primary" onClick={this.enqueueAll}>
+            Start job
+          </Button>
+        </div>
       </div>
     );
   }
