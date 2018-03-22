b15393eb7fcd1fca547afe1932e7f5f9eeca3064
Morten Bøgeskov <mb@dbc.dk>
2018-03-22 13:26:24 +0100

    Use new table to update classifier.

---
+++ service/src/main/resources/db/migration/V5__Classifier-with-data-run-by-hand.sql
@@ -0,0 +1,48 @@
+DO
+$$
+DECLARE
+    hasnt BOOLEAN;
+BEGIN
+    SELECT COUNT(*) = 0 INTO hasnt FROM information_schema.columns WHERE table_name = 'bibliographicsolrkeys' AND column_name = 'classifier';
+    IF hasnt THEN
+
+        ALTER TABLE bibliographicSolrKeys
+          DROP CONSTRAINT bibliographicSolrKeys_pkey;
+        DROP INDEX bibliographicsolrkeysjson;
+        ALTER TABLE bibliographicsolrkeys
+          RENAME TO bibliographicsolrkeys_old;
+
+        CREATE TABLE bibliographicsolrkeys AS
+          SELECT agencyid, CAST('UNKNOWN' AS VARCHAR(16)) AS classifier, bibliographicrecordid, work, unit, producerversion, deleted, indexkeys, trackingid
+          FROM bibliographicsolrkeys_old WITH DATA;
+
+        ALTER TABLE bibliographicSolrKeys
+          ALTER COLUMN agencyid SET NOT NULL;
+        ALTER TABLE bibliographicSolrKeys
+          ALTER COLUMN classifier SET NOT NULL;
+        ALTER TABLE bibliographicSolrKeys
+          ALTER COLUMN bibliographicrecordid SET NOT NULL;
+        ALTER TABLE bibliographicSolrKeys
+          ALTER COLUMN producerversion SET NOT NULL;
+        ALTER TABLE bibliographicSolrKeys
+          ALTER COLUMN deleted SET NOT NULL;
+        ALTER TABLE bibliographicSolrKeys
+          ALTER COLUMN trackingid SET NOT NULL;
+        ALTER TABLE bibliographicSolrKeys
+          ALTER COLUMN trackingid SET DEFAULT '';
+
+        DROP TABLE bibliographicsolrkeys_old;
+
+        ALTER TABLE bibliographicSolrKeys
+          ADD CONSTRAINT bibliographicSolrKeys_pkey
+          PRIMARY KEY (agencyId, classifier, bibliographicRecordId);
+        -- SLOW - will time out on cluster health check
+        CREATE INDEX bibliographicsolrkeysjson
+--        CREATE INDEX CONCURRENTLY bibliographicsolrkeysjson
+          ON bibliographicsolrkeys
+          USING GIN (indexkeys jsonb_path_ops)
+          WHERE indexkeys IS NOT NULL;
+
+    END IF;
+END
+$$;
