e6bf2768be4290170e845f7edd88c29ce3095782
Morten Bøgeskov <mb@dbc.dk>
2018-04-24 15:21:50 +0200

    Introduce field hasLiveHoldings in h. Indicating if has enything with status <> decommissioned

--- service/src/test/resources/entityTestData.sql
+++ service/src/test/resources/entityTestData.sql
@@ -2,11 +2,11 @@ INSERT INTO bibliographicSolrKeys ( AGENCYID, CLASSIFIER, BIBLIOGRAPHICRECORDID,
      VALUES ( 300, 'clazzifier', '4321', 'id#1', TRUE , '{"ti": ["isdnBogen", "title2"] , "001": ["argle"] }'::jsonb, '5544','track', 'unit:3', 'work:3');
 
 
-INSERT INTO holdingsitemssolrkeys (AGENCYID, BIBLIOGRAPHICRECORDID, INDEXKEYS, PRODUCERVERSION, TRACKINGID)
-     VALUES ( 300, '4321', '[{"id": ["argle"], "title": ["unix bogen"]}, {"id": ["argle"], "dyr": ["hest"], "title": ["unix bogen"]}]'::jsonb, 'revision','track');
+INSERT INTO holdingsitemssolrkeys (AGENCYID, BIBLIOGRAPHICRECORDID, HASLIVEHOLDINGS, INDEXKEYS, PRODUCERVERSION, TRACKINGID)
+     VALUES ( 300, '4321', TRUE, '[{"id": ["argle"], "title": ["unix bogen"]}, {"id": ["argle"], "dyr": ["hest"], "title": ["unix bogen"]}]'::jsonb, 'revision','track');
 
-INSERT INTO holdingsitemssolrkeys (AGENCYID, BIBLIOGRAPHICRECORDID, INDEXKEYS, PRODUCERVERSION, TRACKINGID)
-     VALUES ( 600, '600', '[{"id": ["argle"], "title": ["unix bogen"]}, {"id": ["argle"], "dyr": ["hest"], "title": ["unix bogen"]}]'::jsonb, 'revision','track');
+INSERT INTO holdingsitemssolrkeys (AGENCYID, BIBLIOGRAPHICRECORDID, HASLIVEHOLDINGS, INDEXKEYS, PRODUCERVERSION, TRACKINGID)
+     VALUES ( 600, '600', FALSE, '[{"id": ["argle"], "title": ["unix bogen"]}, {"id": ["argle"], "dyr": ["hest"], "title": ["unix bogen"]}]'::jsonb, 'revision','track');
 
 
 INSERT INTO holdingsTobibliographic (holdingsagencyid,holdingsbibliographicrecordid, bibliographicagencyid, bibliographicrecordid, isCommonDerived ) VALUES (600, '600', 100,'600', false);
