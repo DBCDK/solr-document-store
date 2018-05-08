63afc5b0d2c854701bcfb0ff74f8310513730933
Morten Bøgeskov <mb@dbc.dk>
2018-05-08 10:32:21 +0200

    Keep db connection ready for status check.

--- solr-updater/src/main/docker/config.d/01_setup_db_env.sh
+++ solr-updater/src/main/docker/config.d/01_setup_db_env.sh
@@ -1,1 +1,1 @@
-add_jdbc_resource_from_url "jdbc/solr-doc-store" ${SOLR_DOC_STORE_DATABASE} max-pool-size=${THREADS:-2} steady-pool-size=$(( (1 + ${THREADS:-2}) / 2))
+add_jdbc_resource_from_url "jdbc/solr-doc-store" ${SOLR_DOC_STORE_DATABASE} max-pool-size=$((${THREADS:-2} + 1)) steady-pool-size=$(( (1 + ${THREADS:-2}) / 2))
