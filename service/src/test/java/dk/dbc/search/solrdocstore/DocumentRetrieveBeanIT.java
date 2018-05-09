d6255c466826d4e640ffd032b1ab8bb5e1b445b2
Morten Bøgeskov <mb@dbc.dk>
2018-05-09 14:45:23 +0200

    dkcclterm.ln with rec.exclude...

--- service/src/test/java/dk/dbc/search/solrdocstore/DocumentRetrieveBeanIT.java
+++ service/src/test/java/dk/dbc/search/solrdocstore/DocumentRetrieveBeanIT.java
@@ -8,6 +8,12 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static dk.dbc.search.solrdocstore.BeanFactoryUtil.*;
+import static dk.dbc.search.solrdocstore.OpenAgencyUtil.*;
 
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
@@ -21,10 +27,20 @@ import org.hamcrest.Matchers;
 public class DocumentRetrieveBeanIT extends JpaSolrDocStoreIntegrationTester {
 
     private static final ObjectMapper O = new ObjectMapper();
+
     private static final List<Map<String, List<String>>> DECOMMISSIONED = indexKeys("[{\"holdingsitem.status\":[\"Decommissioned\"]}]");
     private static final List<Map<String, List<String>>> ON_SHELF = indexKeys("[{\"holdingsitem.status\":[\"OnShelf\"]}]");
+    private static final Map<String, List<String>> EXCLUDE = indexKeysB("{\"rec.excludeFromUnionCatalogue\":[\"true\"]}");
+    private static final Map<String, List<String>> NOTEXCLUDE = indexKeysB("{\"rec.excludeFromUnionCatalogue\":[\"false\"]}");
+    private static final Map<String, List<String>> NOEXCLUDE = indexKeysB("{}");
+    private static final Map<String, List<String>> EMPTY = indexKeysB("{}");
+    private static final String ID = "ABC";
+
     private DocumentRetrieveBean bean;
     private EntityManager em;
+    private BibliographicBean bibl;
+    private HoldingsItemBean hold;
+    private HoldingsToBibliographicBean h2b;
 
     @Before
     public void setupBean() {
@@ -32,7 +48,13 @@ public class DocumentRetrieveBeanIT extends JpaSolrDocStoreIntegrationTester {
         JpaTestEnvironment env = env();
         em = env.getEntityManager();
         bean.entityManager = em;
-        bean.brBean = BeanFactoryUtil.createBibliographicRetrieveBean(env);
+        bean.brBean = createBibliographicRetrieveBean(env);
+        bibl = createBibliographicBean(env);
+        hold = createHoldingsItemBean(env);
+        h2b = createHoldingsToBibliographicBean(env);
+        env().getPersistenceContext().run(() -> {
+            bibl.addBibliographicKeys(new BibliographicEntity(COMMON_AGENCY, "basis", ID, "r:0", "w:0", "u:0", "v0", false, EMPTY, "t0"), Arrays.asList("CBA"));
+        });
     }
 
     @Test
@@ -56,92 +78,56 @@ public class DocumentRetrieveBeanIT extends JpaSolrDocStoreIntegrationTester {
         System.out.println("getPartOfDanbibCommon");
         List<Integer> agencies = env().getPersistenceContext().run(() -> {
 
-            // School part of danbib derived from common
-            em.persist(new OpenAgencyEntity(311111, LibraryType.FBSSchool, false, false));
-            em.persist(new HoldingsItemEntity(311111, "a", "", ON_SHELF, ""));
-            em.persist(new HoldingsToBibliographicEntity(311111, "a", 870970, "a", true));
+            build(300055).holdings(ON_SHELF);
+            build(800055).holdings(ON_SHELF);
+            build(710001, "CBA").holdings(ON_SHELF);
+            build(710002, "CBA").holdings(DECOMMISSIONED);
 
-            // Fbs part of danbib from common (*)
-            em.persist(new OpenAgencyEntity(711111, LibraryType.FBS, false, true));
-            em.persist(new HoldingsItemEntity(711111, "a", "", ON_SHELF, ""));
-            em.persist(new HoldingsToBibliographicEntity(711111, "a", 870970, "a", true));
+            build(700011).holdings(ON_SHELF);
+            build(700015).holdings(ON_SHELF);
+            build(700051).holdings(ON_SHELF);
+            build(700055).holdings(ON_SHELF);
 
-            // Fbs part of danbib from common - no live holdings
-            em.persist(new OpenAgencyEntity(771111, LibraryType.FBS, false, true));
-            em.persist(new HoldingsItemEntity(771111, "a", "", DECOMMISSIONED, ""));
-            em.persist(new HoldingsToBibliographicEntity(771111, "a", 870970, "a", true));
+            build(700211).record(NOEXCLUDE).holdings(ON_SHELF);
+            build(700215).record(NOEXCLUDE).holdings(ON_SHELF);
+            build(700251).record(NOEXCLUDE).holdings(ON_SHELF);
+            build(700255).record(NOEXCLUDE).holdings(ON_SHELF);
 
-            // Fbs part of danbib from common - superceeded (*)
-            em.persist(new OpenAgencyEntity(777111, LibraryType.FBS, false, true));
-            em.persist(new HoldingsItemEntity(777111, "b", "", ON_SHELF, ""));
-            em.persist(new HoldingsToBibliographicEntity(777111, "b", 870970, "a", true));
+            build(700311).record(NOTEXCLUDE).holdings(ON_SHELF);
+            build(700315).record(NOTEXCLUDE).holdings(ON_SHELF);
+            build(700351).record(NOTEXCLUDE).holdings(ON_SHELF);
+            build(700355).record(NOTEXCLUDE).holdings(ON_SHELF);
 
-            // Fbs part of danbib from common - own datastream (*)
-            em.persist(new OpenAgencyEntity(777711, LibraryType.FBS, false, true));
-            em.persist(new HoldingsItemEntity(777711, "a", "", ON_SHELF, ""));
-            em.persist(new HoldingsToBibliographicEntity(777711, "a", 777711, "a", true));
+            build(700411).record(EXCLUDE).holdings(ON_SHELF);
+            build(700415).record(EXCLUDE).holdings(ON_SHELF);
+            build(700451).record(EXCLUDE).holdings(ON_SHELF);
+            build(700455).record(EXCLUDE).holdings(ON_SHELF);
 
-            // Fbs part of danbib own record (invalid id)
-            em.persist(new OpenAgencyEntity(777771, LibraryType.FBS, false, true));
-            em.persist(new HoldingsItemEntity(777771, "a", "", ON_SHELF, ""));
-            em.persist(new HoldingsToBibliographicEntity(777771, "a", 777771, "a", false));
-
-            // Fbs from common
-            em.persist(new OpenAgencyEntity(811111, LibraryType.FBS, false, false));
-            em.persist(new HoldingsItemEntity(811111, "a", "", ON_SHELF, ""));
-            em.persist(new HoldingsToBibliographicEntity(811111, "a", 870970, "a", true));
-
-            // Fbs part of danbib own record
-            em.persist(new OpenAgencyEntity(881111, LibraryType.FBS, false, true));
-            em.persist(new HoldingsItemEntity(881111, "a", "", ON_SHELF, ""));
-            em.persist(new HoldingsToBibliographicEntity(881111, "a", 881111, "a", false));
-
-            return bean.getPartOfDanbibCommon("a");
-
+            return bean.getPartOfDanbibCommon(ID);
         });
-        Collections.sort(agencies);
         System.out.println("agencies = " + agencies);
-        assertThat(agencies, Matchers.is(Arrays.asList(711111, 777111, 777711)));
+        assertThat(agencies, Matchers.containsInAnyOrder(710001,
+                                                         700011, 700015, 700051,
+                                                         700211, 700215, 700251,
+                                                         700311, 700315, 700351));
     }
 
     @Test
-    public void getPartOfDanbibSchool() throws Exception {
-        System.out.println("getPartOfDanbib-School");
-        List<Integer> agencies = env().getPersistenceContext().run(() -> {
-            em.persist(new OpenAgencyEntity(311111, LibraryType.FBSSchool, false, false));
-            em.persist(new HoldingsItemEntity(311111, "a", "", ON_SHELF, ""));
-            em.persist(new HoldingsToBibliographicEntity(311111, "a", 870970, "a", true));
-            return bean.getPartOfDanbib(311111, "a");
+    public void getDocumentWithHoldings() throws Exception {
+        System.out.println("getDocumentWithHoldings");
+        DocumentRetrieveResponse resp = env().getPersistenceContext().run(() -> {
+            build(300055).holdings(ON_SHELF);
+            build(800055).holdings(ON_SHELF);
+            build(710001, "CBA").holdings(ON_SHELF);
+            return bean.getDocumentWithHoldingsitems(COMMON_AGENCY, "basis", ID);
         });
-        assertThat(agencies, Matchers.empty());
+        System.out.println("resp = " + resp);
+        Set<String> holdings = resp.holdingsItemRecords.stream()
+                .map(h -> h.getAgencyId() + "-" + h.getBibliographicRecordId())
+                .collect(Collectors.toSet());
+        assertThat(holdings, Matchers.containsInAnyOrder("300055-ABC", "710001-CBA"));
     }
 
-    @Test
-    public void getPartOfDanbibNotDanbib() throws Exception {
-        System.out.println("getPartOfDanbib-not-partOfDanbib");
-        List<Integer> agencies = env().getPersistenceContext().run(() -> {
-            // Fbs part of danbib own record
-            em.persist(new OpenAgencyEntity(881111, LibraryType.FBS, false, false));
-            em.persist(new HoldingsItemEntity(881111, "a", "", ON_SHELF, ""));
-            em.persist(new HoldingsToBibliographicEntity(881111, "a", 881111, "a", false));
-            return bean.getPartOfDanbib(881111, "a");
-        });
-        assertThat(agencies, Matchers.empty());
-    }
-
-
-    @Test
-    public void getPartOfDanbibLocalStream() throws Exception {
-        System.out.println("getPartOfDanbib-LocalStream");
-        List<Integer> agencies = env().getPersistenceContext().run(() -> {
-            // Fbs part of danbib from common - own datastream (*)
-            em.persist(new OpenAgencyEntity(777711, LibraryType.FBS, false, true));
-            em.persist(new HoldingsItemEntity(777711, "a", "", ON_SHELF, ""));
-            em.persist(new HoldingsToBibliographicEntity(777711, "a", 777711, "a", true));
-            return bean.getPartOfDanbib(777711, "a");
-        });
-        assertThat(agencies, Matchers.is(Arrays.asList(777711)));
-    }
 
     private static List<Map<String, List<String>>> indexKeys(String json) {
         try {
@@ -149,6 +135,48 @@ public class DocumentRetrieveBeanIT extends JpaSolrDocStoreIntegrationTester {
                        });
         } catch (IOException ex) {
             throw new RuntimeException(ex);
+        }
+    }
+
+    private static Map<String, List<String>> indexKeysB(String json) {
+        try {
+            return O.readValue(json, new TypeReference<Map<String, List<String>>>() {
+                       });
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    private Build build(int holdingsAgencyId) {
+        return new Build(holdingsAgencyId, ID);
+    }
+
+    private Build build(int holdingsAgencyId, String id) {
+        return new Build(holdingsAgencyId, id);
+    }
+
+    private static final Optional<Integer> NOW = Optional.ofNullable(null);
+
+    private class Build {
+
+        private final int holdingsAgencyId;
+        private final String holdingsId;
+
+        private Build(int holdingsAgencyId, String holdingsId) {
+            this.holdingsAgencyId = holdingsAgencyId;
+            this.holdingsId = holdingsId;
+            em.persist(makeOpenAgencyEntity(holdingsAgencyId));
+        }
+
+        private Build holdings(List<Map<String, List<String>>> content) {
+            hold.setHoldingsKeys(new HoldingsItemEntity(holdingsAgencyId, holdingsId, "v0", content, "t1"), NOW);
+            h2b.tryToAttachToBibliographicRecord(holdingsAgencyId, holdingsId);
+            return this;
+        }
+
+        private Build record(Map<String, List<String>> content) {
+            bibl.addBibliographicKeys(new BibliographicEntity(holdingsAgencyId, "katalog", holdingsId, "r:*", "w:*", "u:*", "v0", false, content, "t0"), Collections.EMPTY_LIST);
+            return this;
         }
     }
 
