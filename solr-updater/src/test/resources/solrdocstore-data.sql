6d04f568276c298161291fe2ad486c99f858f166
Morten Bøgeskov <mb@dbc.dk>
2018-05-24 09:05:43 +0200

    Test data

--- solr-updater/src/test/resources/solrdocstore-data.sql
+++ solr-updater/src/test/resources/solrdocstore-data.sql
@@ -5,12 +5,13 @@ CREATE TABLE OpenAgencyCacheDefault (
     partOfDanbib BOOLEAN NOT NULL,
     authCreateCommonRecord BOOLEAN NOT NULL,
     fetched TIMESTAMP NOT NULL,
+    valid BOOLEAN NOT NULL,
     PRIMARY KEY (agencyId)
 );
 
-INSERT INTO OpenAgencyCacheDefault VALUES(300101, 'FBSSchool', False, False, now());
-INSERT INTO OpenAgencyCacheDefault VALUES(300102, 'FBSSchool', False, False, now());
-INSERT INTO OpenAgencyCacheDefault VALUES(300103, 'FBSSchool', False, False, now());
-INSERT INTO OpenAgencyCacheDefault VALUES(300010, 'FBSSchool', False, False, now());
-INSERT INTO OpenAgencyCacheDefault VALUES(300000, 'FBSSchool', False, False, now());
-INSERT INTO OpenAgencyCacheDefault VALUES(777777, 'FBS', True, True, now());
+INSERT INTO OpenAgencyCacheDefault VALUES(300101, 'FBSSchool', False, False, now(), TRUE);
+INSERT INTO OpenAgencyCacheDefault VALUES(300102, 'FBSSchool', False, False, now(), TRUE);
+INSERT INTO OpenAgencyCacheDefault VALUES(300103, 'FBSSchool', False, False, now(), TRUE);
+INSERT INTO OpenAgencyCacheDefault VALUES(300010, 'FBSSchool', False, False, now(), TRUE);
+INSERT INTO OpenAgencyCacheDefault VALUES(300000, 'FBSSchool', False, False, now(), TRUE);
+INSERT INTO OpenAgencyCacheDefault VALUES(777777, 'FBS', True, True, now(), TRUE);
