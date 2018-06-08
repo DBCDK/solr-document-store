fcb22701d8b6a875918f264edd7cc21149cc48c1
Morten Bøgeskov <mb@dbc.dk>
2018-06-08 12:47:09 +0200

    Openagency retry

--- solr-updater/src/main/java/dk/dbc/search/solrdocstore/updater/OpenAgencyHttp.java
+++ solr-updater/src/main/java/dk/dbc/search/solrdocstore/updater/OpenAgencyHttp.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import java.io.IOException;
 import java.net.URI;
+import java.util.concurrent.TimeUnit;
 import javax.annotation.PostConstruct;
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
@@ -13,6 +14,8 @@ import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import net.jodah.failsafe.Failsafe;
+import net.jodah.failsafe.RetryPolicy;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -32,6 +35,11 @@ public class OpenAgencyHttp {
 
     private Client client;
 
+    private static final RetryPolicy RETRY_POLICY = new RetryPolicy()
+            .withDelay(250, TimeUnit.MILLISECONDS)
+            .retryOn(Exception.class)
+            .withMaxRetries(4);
+
     public OpenAgencyHttp() {
     }
 
@@ -41,6 +49,10 @@ public class OpenAgencyHttp {
     }
 
     public ObjectNode fetchJson(URI uri) throws IOException {
+        return Failsafe.with(RETRY_POLICY).get(() -> fetchJsonImpl(uri));
+    }
+
+    private ObjectNode fetchJsonImpl(URI uri) throws IOException {
         Response response = client.target(uri)
                 .request(MediaType.APPLICATION_JSON_TYPE)
                 .buildGet()
@@ -48,13 +60,13 @@ public class OpenAgencyHttp {
         if (response.getStatus() != 200) {
             throw new IOException("Got status:" + response.getStatusInfo() + " from uri: " + uri);
         }
-        if(!response.getMediaType().equals(MediaType.APPLICATION_JSON_TYPE)) {
-            throw new IOException("Got media type:" + response.getMediaType()+ " from uri: " + uri);
+        if (!response.getMediaType().equals(MediaType.APPLICATION_JSON_TYPE)) {
+            throw new IOException("Got media type:" + response.getMediaType() + " from uri: " + uri);
         }
         String content = response.readEntity(String.class);
         log.trace("content = {}", content);
         JsonNode tree = O.readTree(content);
-        if(!tree.isObject()) {
+        if (!tree.isObject()) {
             throw new IOException("Got content from: " + uri + " but it's not a json object");
         }
         return (ObjectNode) tree;
