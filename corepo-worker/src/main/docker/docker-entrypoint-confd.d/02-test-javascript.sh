e38a6f11faa7cfdb403a2a69678567a1677d4ec4
Morten Bøgeskov <mb@dbc.dk>
2018-08-17 08:55:48 +0200

    Removed old payara4 image directories

--- corepo-worker/src/main/docker/docker-entrypoint-confd.d/02-test-javascript.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -x
-
-if [ -d /javascript ]; then
-    echo "IMPORTING JAVASCRIPT"
-    cp -avf /javascript/. $PAYARA_HOME/domain1/applications/corepo-worker/WEB-INF/classes/javascript
-fi
