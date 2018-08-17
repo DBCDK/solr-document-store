8b282d1dcde75133bfb18db4c2eba8b44b2fce58
Morten Bøgeskov <mb@dbc.dk>
2018-08-17 08:49:35 +0200

    Correct JavaScript path in war file

--- corepo-worker/src/main/docker/startup.d/02-test-javascript.sh
+++ corepo-worker/src/main/docker/startup.d/02-test-javascript.sh
@@ -2,5 +2,7 @@
 
 if [ -d /javascript ]; then
     echo "IMPORTING JAVASCRIPT"
-    (cd / && zip -ur0 --exclude=*/.svn/* --exclude=*/.git/* --exclude=*.test.js $PAYARA_CFG_DIR/app.war javascript)
+    mkdir -p /tmp/WEB-INF/classes/
+    cp -a /javascript /tmp/WEB-INF/classes/
+    (cd /tmp && zip -ur0 --exclude=*/.svn/* --exclude=*/.git/* --exclude=*.test.js $PAYARA_CFG_DIR/app.war WEB-INF)
 fi
