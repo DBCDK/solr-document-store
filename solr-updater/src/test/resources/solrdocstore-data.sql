f922f7ef0b3211193ebca4fb65c94d55ef4c30f0
Morten Bøgeskov <mb@dbc.dk>
2020-02-04 08:02:06 +0100

    Updated test data

--- solr-updater/src/test/resources/solrdocstore-data.sql
+++ solr-updater/src/test/resources/solrdocstore-data.sql
@@ -2,6 +2,7 @@
 CREATE TABLE OpenAgencyCacheDefault (
     agencyId NUMERIC(6) NOT NULL,
     libraryType TEXT NOT NULL,
+    partOfBibDk BOOLEAN NOT NULL,
     partOfDanbib BOOLEAN NOT NULL,
     authCreateCommonRecord BOOLEAN NOT NULL,
     fetched TIMESTAMP NOT NULL,
@@ -9,9 +10,10 @@ CREATE TABLE OpenAgencyCacheDefault (
     PRIMARY KEY (agencyId)
 );
 
-INSERT INTO OpenAgencyCacheDefault VALUES(300101, 'FBSSchool', False, False, now(), TRUE);
-INSERT INTO OpenAgencyCacheDefault VALUES(300102, 'FBSSchool', False, False, now(), TRUE);
-INSERT INTO OpenAgencyCacheDefault VALUES(300103, 'FBSSchool', False, False, now(), TRUE);
-INSERT INTO OpenAgencyCacheDefault VALUES(300010, 'FBSSchool', False, False, now(), TRUE);
-INSERT INTO OpenAgencyCacheDefault VALUES(300000, 'FBSSchool', False, False, now(), TRUE);
-INSERT INTO OpenAgencyCacheDefault VALUES(777777, 'FBS', True, True, now(), TRUE);
+INSERT INTO OpenAgencyCacheDefault (agencyid, librarytype, partofbibdk, partofdanbib, authcreatecommonrecord, fetched, valid) VALUES
+ (300101, 'FBSSchool', False, False, False, now(), TRUE),
+ (300102, 'FBSSchool', False, False, False, now(), TRUE),
+ (300103, 'FBSSchool', False, False, False, now(), TRUE),
+ (300010, 'FBSSchool', False, False, False, now(), TRUE),
+ (300000, 'FBSSchool', False, False, False, now(), TRUE),
+ (777777, 'FBS', True, True, True, now(), TRUE);
