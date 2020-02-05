7cead66b9203976c118fa214e1ca72e761ed6bf8
Morten Bøgeskov <mb@dbc.dk>
2020-02-05 11:07:15 +0100

    Unused Imports

--- service/src/main/java/dk/dbc/search/solrdocstore/QueueRulesDaemon.java
+++ service/src/main/java/dk/dbc/search/solrdocstore/QueueRulesDaemon.java
@@ -22,7 +22,6 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
