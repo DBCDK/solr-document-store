54297ecf8ac8fa31f96ec4a0b8cb518dbe16c081
Morten Bøgeskov <mb@dbc.dk>
2021-03-26 09:14:23 +0100

    postgresql image with new queuerules

--- postgresql/src/main/docker/docker-entrypoint.d/10-queueruledefault.sh
+++ postgresql/src/main/docker/docker-entrypoint.d/10-queueruledefault.sh
@@ -2,7 +2,7 @@
 
 (
     echo "\set ON_ERROR_STOP"
-    for queue in $(echo ${SOLR_DOC_STORE_QUEUE_NAMES//[^-0-9a-zA-Z]/ }); do
-        echo "INSERT INTO queuerule(queue) values('$queue');"
+    for queue in $(echo ${SOLR_DOC_STORE_QUEUE_NAMES//,/ }); do
+        echo "INSERT INTO queuerule(queue, supplier, postpone) values('${queue//|/\', \'}');"
     done
 ) | PGPASSWORD=$POSTGRES_PASSWORD psql $POSTGRES_USER -d $POSTGRES_DB
