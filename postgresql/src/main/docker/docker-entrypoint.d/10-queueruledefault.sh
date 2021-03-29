8ab889ddd59d1e66c85d616bc09e52f3b329fe6b
Morten Bøgeskov <mb@dbc.dk>
2021-03-29 11:39:03 +0200

    Renamed rules to SOLR_DOC_STORE_QUEUES from postgresql image

--- postgresql/src/main/docker/docker-entrypoint.d/10-queueruledefault.sh
+++ postgresql/src/main/docker/docker-entrypoint.d/10-queueruledefault.sh
@@ -2,7 +2,7 @@
 
 (
     echo "\set ON_ERROR_STOP"
-    for queue in $(echo ${SOLR_DOC_STORE_QUEUE_NAMES//,/ }); do
+    for queue in $(echo ${SOLR_DOC_STORE_QUEUES//,/ }); do
         echo "INSERT INTO queuerule(queue, supplier, postpone) values('${queue//|/\', \'}');"
     done
 ) | PGPASSWORD=$POSTGRES_PASSWORD psql $POSTGRES_USER -d $POSTGRES_DB
