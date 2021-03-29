f2595a407fbec77c5efb7b3038bedbf05d394f36
Morten Bøgeskov <mb@dbc.dk>
2021-03-29 11:20:44 +0200

    Foreign key constraint on queuerule supplier

--- db/src/main/resources/db/migration/V21__QueueRulesWithSupplier.sql
+++ db/src/main/resources/db/migration/V21__QueueRulesWithSupplier.sql
@@ -1,5 +1,19 @@
+CREATE TABLE queuesuppliers (
+  supplier TEXT NOT NULL PRIMARY KEY
+);
+INSERT INTO queuesuppliers VALUES('manifestation');
+INSERT INTO queuesuppliers VALUES('manifestation_deleted');
+INSERT INTO queuesuppliers VALUES('holding');
+INSERT INTO queuesuppliers VALUES('firstlastholding');
+INSERT INTO queuesuppliers VALUES('resource');
+INSERT INTO queuesuppliers VALUES('work');
+INSERT INTO queuesuppliers VALUES('workfirstlastholding');
+
 ALTER TABLE queuerule
   ADD COLUMN supplier TEXT NOT NULL DEFAULT 'manifestation';
+
+ALTER TABLE queuerule
+  ADD CONSTRAINT queuerule_supplier_fkey FOREIGN KEY (supplier) REFERENCES queuesuppliers (supplier);
 
 ALTER TABLE queuerule
   ALTER COLUMN supplier DROP DEFAULT;
@@ -10,6 +24,8 @@ ALTER TABLE queuerule
   ADD CONSTRAINT queuerule_pkey PRIMARY KEY (queue, supplier);
 
 INSERT INTO queuerule(queue, supplier) SELECT queue, 'holding' FROM queuerule WHERE supplier = 'manifestation';
+INSERT INTO queuerule(queue, supplier) SELECT queue, 'resource' FROM queuerule WHERE supplier = 'manifestation';
+INSERT INTO queuerule(queue, supplier) SELECT queue, 'manifestation_deleted' FROM queuerule WHERE supplier = 'manifestation';
 
 ALTER TABLE queuerule
   ADD COLUMN postpone INTEGER NOT NULL DEFAULT 0;
