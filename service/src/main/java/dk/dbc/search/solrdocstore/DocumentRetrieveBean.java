8b5bf30852d4d12d4c90aa2ef2b9f9a5b4f2e74a
Morten Bøgeskov <mb@dbc.dk>
2022-09-07 12:15:25 +0200

    PMD

--- service/src/main/java/dk/dbc/search/solrdocstore/DocumentRetrieveBean.java
+++ service/src/main/java/dk/dbc/search/solrdocstore/DocumentRetrieveBean.java
@@ -29,7 +29,6 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
